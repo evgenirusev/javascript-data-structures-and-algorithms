7415dfa16e05b05ec0e968c43cc5fdfbf5d18e11
rusev9 <evg.rusev@gmail.com>
2020-02-15 12:44:45 +0200

    Implement insert

--- Heaps/MinHeap/MinHeap.js
+++ Heaps/MinHeap/MinHeap.js
@@ -1,8 +1,43 @@
 class MinHeap {
-    insert() { }
+    constructor() {
+        this.data = [];
+    }
+
+    insert(val) {
+        this.data.push(val);
+        this._heapify(this.data.length - 1);
+    }
+
+    extractMin() {
+        
+    }
+
     findMin() { }
-    extractMin() { }
     deleteMin() { }
+
+    _heapify(index) {
+        if (!index) {
+            return;
+        }
+
+        const parentIndex = this._getParentIndex(index);
+
+        if (this.data[index] < this.data[parentIndex]) {
+            this._swap(index, parentIndex);
+        }
+
+        this._heapify(parentIndex);
+    }
+
+    _swap(i, j) {
+        const temp = this.data[i];
+        this.data[i] = this.data[j];
+        this.data[j] = temp;
+    }
+
+    _getParentIndex(index) {
+        return parseInt((index - 1) / 2);
+    }
 }
 
 module.exports = MinHeap;
