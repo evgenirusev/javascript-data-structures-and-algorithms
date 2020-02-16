f3a5d8b38ff265e7553a7c8c5f626f9ca02906f6
rusev9 <evg.rusev@gmail.com>
2020-02-16 14:53:04 +0200

    implement buildHeap

--- Sorting/heapSort.js
+++ Sorting/heapSort.js
@@ -1,5 +1,37 @@
 function heapSort(arr) {
-    
+    buildHeap(arr);
+}
+
+function buildHeap(arr) {
+    for(let i = (arr.length / 2) - 1; i >= 0; i--) {
+        heapify(arr, arr.length, i);
+    }
+}
+
+function heapify(arr, size, index) {
+    let largest = index;
+    left = 2 * index + 1;
+    right = 2 * index + 2;
+
+    if (left < size && arr[left] > arr[largest]) {
+        largest = left;
+    }
+
+    if (right < size && arr[right] > arr[largest]) {
+        largest = right;
+    }
+
+    if (index !== largest) {
+        swap(arr, index, largest);
+        heapify(arr, size, largest);
+    }
+}
+
+function swap(arr, a, b) {
+    const temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+    return arr;
 }
 
 module.exports = heapSort;
