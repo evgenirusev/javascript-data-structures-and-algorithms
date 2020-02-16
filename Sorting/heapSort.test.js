39d175960a307212e4245a0ccd907ed137c9e29e
rusev9 <evg.rusev@gmail.com>
2020-02-16 16:08:10 +0200

    Add new requirement - sort desc

--- Sorting/heapSort.test.js
+++ Sorting/heapSort.test.js
@@ -1,9 +1,17 @@
 const heapSort = require("./heapSort");
 
 describe("heapSort", () => {
-    it("should sort the array", () => {
+    it("should sort the array in ascending order", () => {
         const arr = [2, 8, 5, 3, 9, 1];
         const sorted = [1, 2, 3, 5, 8, 9];
+        
+        heapSort(arr);
+        expect(arr).toEqual(sorted);
+    });
+
+    it("should sort the array in descending order", () => {
+        const arr = [2, 8, 5, 3, 9, 1];
+        const sorted = [9, 8, 5, 3, 2, 1];
         
         heapSort(arr);
         expect(arr).toEqual(sorted);
