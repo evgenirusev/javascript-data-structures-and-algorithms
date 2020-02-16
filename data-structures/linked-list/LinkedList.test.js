4be6443db37157cdfcc3c4c68f88039cf71924b1
rusev9 <evg.rusev@gmail.com>
2020-02-16 20:58:35 +0200

    Add unit test for peekHead as well as addToHead

--- data-structures/linked-list/LinkedList.test.js
+++ data-structures/linked-list/LinkedList.test.js
@@ -1,11 +1,21 @@
 const LinkedList = require("./LinkedList");
 
 describe("LinkedList", () => {
-    it("should peek a correct value", () => {
+    it("should peek a correct tail value", () => {
         const linkedList = new LinkedList();
         linkedList.addToTail(1);
         linkedList.addToTail(2);
+        linkedList.addToTail(13);
 
-        expect(linkedList.peekTail()).toBe(2);
+        expect(linkedList.peekTail()).toBe(13);
+    });
+
+    it("should peek a correct head value", () => {
+        const linkedList = new LinkedList();
+        linkedList.addToTail(1);
+        linkedList.addToHead(4);
+        linkedList.addToHead(3);
+
+        expect(linkedList.peekHead()).toBe(4);
     });
 });
