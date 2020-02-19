4ba1fed7f62954065c35527967709860cbd21ae8
rusev9 <evg.rusev@gmail.com>
2020-02-19 21:56:40 +0200

    add unit test for inorder with callbacks

--- data-structures/binary-search-tree/BinarySearchTree.test.js
+++ data-structures/binary-search-tree/BinarySearchTree.test.js
@@ -16,4 +16,22 @@ describe("BinarySearchTree", () => {
 
         expect(bst.findMin()).toBe(1);
     });
+
+    it("should execute the callback for each value in order", () => {
+        const result = [];
+        const mockCallback = (function(val) {
+            const assertionArray = arguments[0];
+            assertionArray.push(val);
+        }).bind(undefined, result);
+
+        const values = [7, 5, 2, 8, 1, 3];
+
+        values.forEach(val => {
+            bst.insert(val);
+        });
+
+        bst.inorder(mockCallback);
+
+        expect(result).toEqual([1, 2, 3, 5, 7, 8]);
+    });
 });
