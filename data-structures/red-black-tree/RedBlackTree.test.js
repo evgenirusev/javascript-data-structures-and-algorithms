49603837dd83be989f970a80005ae67e1a7d893a
rusev9 <evg.rusev@gmail.com>
2020-03-01 23:14:23 +0200

    optimize unit tests

--- data-structures/red-black-tree/RedBlackTree.test.js
+++ data-structures/red-black-tree/RedBlackTree.test.js
@@ -21,12 +21,13 @@ describe("RedBlackTree", () => {
             });
         }
 
-        generateTraversalTest("inorder", [5, 4, 3, 2, 1], [1, 2, 3, 4, 5]);
+        generateTraversalTest("inorder", [6, 5, 4, 3, 2, 1], [1, 2, 3, 4, 5, 6]);
         generateTraversalTest("inorder", [5, 1, 0, 13, 7], [0, 1, 5, 7, 13]);
 
-        generateTraversalTest("preorder", [5, 4, 3, 2, 1], [4, 2, 1, 3, 5]);
+        generateTraversalTest("preorder", [6, 5, 4, 3, 2, 1], [5, 3, 2, 1, 4, 6]);
         generateTraversalTest("preorder", [5, 1, 0, 13, 7], [7, 1, 0, 5, 13]);
 
-        generateTraversalTest("postorder", [5, 4, 3, 2, 1], [1, 3, 2, 5, 4]);
+        generateTraversalTest("postorder", [6, 5, 4, 3, 2, 1], [1, 2, 4, 3, 6, 5]);
+        generateTraversalTest("postorder", [5, 1, 0, 13, 7], [0, 5, 1, 13, 7]);
     });
 });
