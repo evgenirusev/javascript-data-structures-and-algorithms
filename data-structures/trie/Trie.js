3e8021b7d12c68a4ac004a8af72930b51bb51e1c
rusev9 <evg.rusev@gmail.com>
2020-03-08 10:16:37 +0200

    Add Trie unit tests

--- data-structures/trie/Trie.js
+++ data-structures/trie/Trie.js
@@ -1,20 +1,56 @@
 class Node {
-    constructor() {
-        this.root = new Node(null);
-    }
-
-    insertWord() {}
-    deleteWord() {}
-    hasWord() {}
-    getWordWithPrefix() {}
-}
-
-class Trie {
     constructor(char) {
         this.char = char;
         this.isCompletedWord = false;
         this.children = {};
     }
+}
+
+class Trie {
+    constructor() {
+        this.root = new Node(null);
+    }
+
+    insertWord(word) {
+        if (!word) {
+            throw 'Invalid operation!';
+        }
+
+        let node = this.root;
+        for (let i = 0; i < word.length; i++) {
+            const char = word.charAt(i);
+
+            if (!node.children[char]) {
+                node.children[char] = new Node(char);
+            }
+
+            node = node.children[char];
+        }
+
+        node.isCompletedWord = true;
+    }
+
+    hasWord(word) {
+        if (!word) {
+            throw 'Invalid operation!';
+        }
+
+        let node = this.root;
+        for (let i = 0; i < word.length; i++) {
+            const char = word.charAt(i);
+
+            if (!node.children[char]) {
+                return false;
+            }
+
+            node = node.children[char];
+        }
+
+        return node.isCompletedWord;
+    }
+
+    deleteWord() { }
+    getWordsWithPrefix() { }
 }
 
 module.exports = Trie;
--- data-structures/trie/Trie.test.js
+++ data-structures/trie/Trie.test.js
@@ -1,14 +1,31 @@
-import Trie from '../Trie';
+const Trie = require("./Trie");
 
 describe('Trie', () => {
-    it('should insert a word and retrieve it', () => {
-        const trie = new Trie();
-        trie.addWord('foo');
-        trie.addWord('bAr');
-        trie.addWord('VR');
+    let trie;
+
+    beforeEach(() => {
+        trie = new Trie()
+    });
+
+    it('should insert a word and confirm that it exists', () => {
+        trie.insertWord('foo');
+        trie.insertWord('bar');
+        trie.insertWord('abcdefffe');
 
         expect(trie.hasWord('foo')).toBe(true);
-        expect(trie.hasWord('BaR')).toBe(true);
-        expect(trie.hasWord('VR')).toBe(true);
+        expect(trie.hasWord('bar')).toBe(true);
+        expect(trie.hasWord('abcdefffe')).toBe(true);
+    });
+
+    it('should successfuly delete words', () => {
+        trie.insertWord('foo');
+        trie.insertWord('bar');
+
+        trie.deleteWord('foo');
+        expect(trie.hasWord('foo')).toBe(false);
+        expect(trie.hasWord('bar')).toBe(true);
+
+        trie.deleteWord('bar');
+        expect(trie.hasWord('bar')).toBe(false);
     });
 });
