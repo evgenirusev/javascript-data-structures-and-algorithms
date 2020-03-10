9f9dc4aedae9b33b15ac1a8f51c773630b442a82
rusev9 <evg.rusev@gmail.com>
2020-03-10 08:24:33 +0200

    Implement recursive inorder in interval tree

--- data-structures/trees/interval-tree/IntervalTree.js
+++ data-structures/trees/interval-tree/IntervalTree.js
@@ -3,7 +3,10 @@ class IntervalTree {
         this.root = this._insert(this.root, inserval);
     }
 
-    inorder() { }
+    inorder(cb) {
+        this._inorderRecursive(this.root, cb);
+    }
+
     preorder() { }
     postorder() { }
     findAny(start, end) { }
@@ -19,10 +22,20 @@ class IntervalTree {
         } else if (interval.start > node.interval.start) {
             node.right = this._insert(node.right, interval);
         } else {
-            throw 'tree does not support duplicate interval starts!'
+            throw 'tree does not support duplicate interval starts!';
         }
 
         return node;
+    }
+
+    _inorderRecursive(node, cb) {
+        if (!node) {
+            return;
+        }
+
+        this._inorderRecursive(node.left, cb);
+        cb(node.interval);
+        this._inorderRecursive(node.right, cb);
     }
 }
 
