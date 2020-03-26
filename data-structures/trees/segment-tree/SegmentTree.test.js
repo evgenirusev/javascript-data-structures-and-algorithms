6f13dbc4ba1a7d6a40e867508f252998f66983f5
rusev9 <evg.rusev@gmail.com>
2020-03-26 15:35:02 +0200

    Add failing unit test

--- data-structures/trees/segment-tree/SegmentTree.test.js
+++ data-structures/trees/segment-tree/SegmentTree.test.js
@@ -9,4 +9,18 @@ describe("SegmentTree", () => {
 
         expect(segmentTree.rangeQuery(0, array.length - 1)).toBe(15);
     });
+
+    it("should do a range query correctly", () => {
+        const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+        segmentTree = new SegmentTree(array);
+
+        expect(segmentTree.rangeQuery(0, array.length - 1)).toBe(105);
+    });
+
+    it("should do a range query correctly", () => {
+        const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+        segmentTree = new SegmentTree(array);
+
+        expect(segmentTree.rangeQuery(0, 4)).toBe(15);
+    });
 });
