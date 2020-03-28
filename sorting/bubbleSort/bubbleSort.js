e9088cd654c5379e46940ac863a53c800dbfeb2b
rusev9 <evg.rusev@gmail.com>
2020-03-28 22:51:47 +0200

    implement bubbleSort

--- sorting/bubbleSort/bubbleSort.js
+++ sorting/bubbleSort/bubbleSort.js
@@ -1,7 +1,11 @@
 function bubble(arr, cmp) {
+    if (typeof cmp !== "function") {
+        cmp = (a, b) => a - b;
+    }
+
     for (let i = 0; i < arr.length; i++) {
         for (let j = i + 1; j < arr.length; j++) {
-            if (cmp(arr[i], arr[j]) < 0) {
+            if (cmp(arr[i], arr[j]) > 0) {
                 swap(arr, i, j);
             }
         }
