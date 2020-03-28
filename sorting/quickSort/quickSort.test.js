2cb53ab0d86e0a42a5a3fc994d663818873e94bf
rusev9 <evg.rusev@gmail.com>
2020-03-28 23:04:06 +0200

    Add quick sort tests

---
+++ sorting/quickSort/quickSort.test.js
@@ -0,0 +1,21 @@
+const quickSort = require("./quickSort");
+const isSortingAlgorithmValid = require("../../mocks/isSortingAlgorithmValid");
+const arrayFactoryUtils = require("../../mocks/arrayFactoryUtils");
+
+describe("quickSort", () => {
+    function generateSortingTest(arr, cmp) {
+        it("should quickSort the array correctly", () => {
+            expect(
+                isSortingAlgorithmValid(quickSort, arr, cmp)
+            ).toBe(true);
+        });
+    }
+
+    Object.values(arrayFactoryUtils)
+        .forEach((generateArray) => {
+            generateSortingTest(generateArray());
+        });
+
+    const desc = (a, b) => b - a;
+    generateSortingTest(arrayFactoryUtils.getUnsortedArray(), desc);
+});
