1c72f5069e3c7fab9ea75c27f5b92abadb9961cc
rusev9 <evg.rusev@gmail.com>
2020-03-29 20:33:54 +0300

    Insertion sort custom implementation

--- sorting/insertionSort/insertionSort.js
+++ sorting/insertionSort/insertionSort.js
@@ -1,9 +1,23 @@
-function insertionSort(arr, cmp) {
+function insertionSort(array, cmp) {
     if (typeof cmp !== "function") {
         cmp = (a, b) => a - b;
     }
 
-    // TODO: Implement
+    for (let i = 1; i < array.length; i++) {
+        let current = i;
+        while (cmp(array[current], array[current - 1]) < 0) {
+            swap(array, current, current - 1);
+            current--;
+        }
+    }
+
+    return array;
+}
+
+function swap(arr, i, j) {
+    const temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
 }
 
 module.exports = insertionSort;
