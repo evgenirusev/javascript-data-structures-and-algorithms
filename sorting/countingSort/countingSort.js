6dd5cf7795f66ae98f206e8f4746f450c0c85482
rusev9 <evg.rusev@gmail.com>
2020-04-01 22:00:02 +0300

    initial implementation of counting sort

--- sorting/countingSort/countingSort.js
+++ sorting/countingSort/countingSort.js
@@ -1,5 +1,21 @@
-function countingSort(arr) {
+function countingSort(array) {
+    const maxValue = Math.max(...array);
 
+    const countArray = new Array(maxValue + 1).fill(0);
+
+    array.forEach(num => {
+        countArray[num]++;
+    });
+
+    for (let i = 1; i < countArray.length; i++) {
+        countArray[i] = countArray[i] + countArray[i - 1];
+    }
+
+    for (let i = array.length - 1; i > 0; i++) {
+        array[i] = countArray[array[i]];
+    }
+
+    return array;
 }
 
 module.exports = countingSort;
