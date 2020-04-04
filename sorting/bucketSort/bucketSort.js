c54c94a30be85b11382a7ef77cd63cdd917a8da6
rusev9 <evg.rusev@gmail.com>
2020-04-04 10:59:08 +0300

    parse into buckets implementstion

--- sorting/bucketSort/bucketSort.js
+++ sorting/bucketSort/bucketSort.js
@@ -8,4 +8,15 @@ function bucketSort(arr, cmp) {
     return mergeBuckets(buckets);
 }
 
+function parseIntoBuckets(array) {
+    const buckets = [];
+
+    array.forEach(num => {
+        const bucketIndex = Math.floor((arr.length - 1) * num / Math.max(...array));
+        buckets[bucketIndex].push(num);
+    });
+
+    return buckets;
+}
+
 module.exports = bucketSort;
