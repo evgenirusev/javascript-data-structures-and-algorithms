31d36baa3026d68130d6686681ce427636a4d48b
rusev9 <evg.rusev@gmail.com>
2020-04-04 14:29:44 +0300

    implement sorting of the buckets

--- sorting/bucketSort/bucketSort.js
+++ sorting/bucketSort/bucketSort.js
@@ -21,4 +21,32 @@ function parseIntoBuckets(array) {
     return buckets;
 }
 
+function sortBuckets(buckets) {
+    buckets.forEach(bucket => {
+        if (typeof bucket !== undefined) {
+            insertionSort(bucket);
+        }
+    });
+}
+
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        for (let j = i; j >= 0; j--) {
+            if (arr[j] < arr[j - 1]) {
+                swap(arr, j, j - 1);
+            }
+        }
+    }
+}
+
+function swap(arr, index1, index2) {
+    const temp = arr[index1];
+    arr[index1] = index2;
+    arr[index2] = temp;
+}
+
+function mergeBuckets(buckets) {
+
+}
+
 module.exports = bucketSort;
