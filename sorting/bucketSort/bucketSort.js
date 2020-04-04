47c95ec6cf10be6bf79c0d138fa6cceb4c8cff83
rusev9 <evg.rusev@gmail.com>
2020-04-04 10:49:54 +0300

    implement first level of abstraction

--- sorting/bucketSort/bucketSort.js
+++ sorting/bucketSort/bucketSort.js
@@ -2,6 +2,10 @@ function bucketSort(arr, cmp) {
     if (typeof cmp !== "function") {
         cmp = (a, b) => a - b;
     }
+
+    const buckets = parseIntoBuckets(arr);
+    sortBuckets(buckets);
+    return mergeBuckets(buckets);
 }
 
 module.exports = bucketSort;
