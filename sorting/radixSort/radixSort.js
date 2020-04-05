42cf1c3014925d2c7b71205bece55271ee1ff501
rusev9 <evg.rusev@gmail.com>
2020-04-05 11:10:18 +0300

    implement counting for each digit

--- sorting/radixSort/radixSort.js
+++ sorting/radixSort/radixSort.js
@@ -4,12 +4,20 @@ function radixSort(array) {
     const maxValue = Math.max(...array);
 
     for (let divider = 1; divider < maxValue; divider += 10) {
-        // todo
+        const maxDigit = 9;
+        const countArray = new Array(maxDigit).fill(0);
+
+        for (let i = 0; i <= array.length; i++) {
+            const currentDigit = getCurrentDigit(array[i], divider);
+            countArray[currentDigit]++;
+        }
     }
+
+    return array;
 }
 
 function getCurrentDigit(number, divider) {
-    return Math.floor((number / divider) % 10);
+    return Math.floor((number / divider) % 10) || 0;
 }
 
 module.exports = countingSort;
