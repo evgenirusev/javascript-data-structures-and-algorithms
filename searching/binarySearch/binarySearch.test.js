a3458c5e1f2f4d16a87a4df6cac0fbbc228fdb44
rusev9 <evg.rusev@gmail.com>
2020-04-12 22:21:33 +0300

    refactor

--- searching/binarySearch/binarySearch.test.js
+++ searching/binarySearch/binarySearch.test.js
@@ -1,8 +1,8 @@
 const binarySearch = require("./binarySearch");
+const arrayFactoryUtils = require("../../mocks/arrayFactoryUtils");
 
 describe("binarySearch", () => {
     it("should find the correct element", () => {
-        const arr = [1, 2];
-        expect(binarySearch(arr, 1)).toEqual(true);
+        expect(binarySearch(arrayFactoryUtils.getTwoElementArray(), 1)).toEqual(true);
     });
 });
