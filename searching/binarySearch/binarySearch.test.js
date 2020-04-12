bebb865cbf5edb81e34e34002f36461136da8b3c
rusev9 <evg.rusev@gmail.com>
2020-04-12 22:38:55 +0300

    add additonal test case

--- searching/binarySearch/binarySearch.test.js
+++ searching/binarySearch/binarySearch.test.js
@@ -6,19 +6,23 @@ describe("binarySearch", () => {
         expect(binarySearch(arrayFactoryUtils.getTwoElementArray(), 1)).toEqual(true);
     });
 
-    it("should find the correct element", () => {
+    it("should find left most element", () => {
         expect(binarySearch(arrayFactoryUtils.getSortedArray(), 1)).toEqual(true);
     });
 
-    it("should find the correct element", () => {
+    it("should't find non existing element", () => {
         expect(binarySearch(arrayFactoryUtils.getSortedArray(), 50)).toEqual(false);
     });
 
-    it("should find the correct element", () => {
+    it("should find the", () => {
         expect(binarySearch(arrayFactoryUtils.getSortedArray(), 3)).toEqual(true);
     });
 
     it("should find the correct element", () => {
         expect(binarySearch(arrayFactoryUtils.getSortedArray(), 20)).toEqual(true);
+    });
+
+    it("should't find element non existing element", () => {
+        expect(binarySearch(arrayFactoryUtils.getSortedArray(), 0)).toEqual(false);
     });
 });
