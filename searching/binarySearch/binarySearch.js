85745dcd481f1476b65d566fa618215d7930461f
rusev9 <evg.rusev@gmail.com>
2020-04-12 22:08:46 +0300

    implement binary search

--- searching/binarySearch/binarySearch.js
+++ searching/binarySearch/binarySearch.js
@@ -1,5 +1,27 @@
-function binarySearch(arr, el) {
+function binarySearch(arr, num, cmp) {
+    if (typeof cmp !== "function") {
+        cmp = (a, b) => a - b;
+    }
 
+    return binarySearchRecursive(arr, cmp, num, 0, arr.length - 1);
+}
+
+function binarySearchRecursive(arr, cmp, num, left, right) {
+    if (right <= right) {
+        return false;
+    }
+
+    const mid = Math.floor(left, right);
+
+    if (num === arr[mid]) {
+        return true;
+    }
+
+    if (cmp(num, arr[mid]) > 0) {
+        return binarySearchRecursive(arr, cmp, num, mid, right);
+    } else {
+        return binarySearchRecursive(arr, cmp, num, left, mid);
+    }
 }
 
 module.exports = binarySearch;
