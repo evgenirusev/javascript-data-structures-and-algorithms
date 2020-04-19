a8733507f295d53079941b783df4b7e282d2fe09
rusev9 <evg.rusev@gmail.com>
2020-04-19 18:08:24 +0300

    Add implementation for union find

--- data-structures/union-find/UnionFind.js
+++ data-structures/union-find/UnionFind.js
@@ -1,5 +1,32 @@
 class UnionFind {
+    constructor(nodes) {
+        this.IDsToNodesMap = nodes.reduce((acc, node) => {
+            acc[node.id] = {
+                node,
+                setId: node.id
+            }
 
+            return acc;
+        }, {});
+    }
+
+    union(id1, id2) {
+        if (this.IDsToNodesMap.id1 || this.IDsToNodesMap.id2) {
+            throw 'passed ID does not exist!'; 
+        }
+
+        const setId = this.IDsToNodesMap[id1].setId;
+
+        Object.values(this.IDsToNodesMap)
+            .filter(node => node.setId === setId)
+            .forEach(node => {
+                node.setId = id2;
+            });
+    }
+
+    areConnected(id1, id2) {
+        return this.IDsToNodesMap[id1].setId === this.IDsToNodesMap[id2].setId;
+    }
 }
 
 module.exports = UnionFind;
--- data-structures/union-find/UnionFind.test.js
+++ data-structures/union-find/UnionFind.test.js
@@ -2,7 +2,18 @@ const UnionFind = require("./UnionFind");
 
 describe('UnionFind', () => {
     it('should be connected', () => {
-        const unionFind = new UnionFind(5);
+        const nodes = [
+            {
+                id: 1,
+                data: 123
+            },
+            {
+                id: 2,
+                data: 123
+            }
+        ];
+
+        const unionFind = new UnionFind(nodes);
         unionFind.union(1, 2);
         expect(unionFind.areConnected(1, 2)).toBe(true);
     });
