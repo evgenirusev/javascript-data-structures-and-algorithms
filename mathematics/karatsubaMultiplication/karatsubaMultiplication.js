66ff29b2f75fa6d0828d7db485e8fc9c5f3d7b14
rusev9 <evg.rusev@gmail.com>
2020-04-25 00:08:29 +0300

    update

--- mathematics/karatsubaMultiplication/karatsubaMultiplication.js
+++ mathematics/karatsubaMultiplication/karatsubaMultiplication.js
@@ -1,5 +1,17 @@
 function karatsubaMultiplication(x, y) {
-    
+    const xDigitCount = getDigitCount(x);
+    const yDigitCount = getDigitCount(y);
+
+    if (xDigitCount < 1 && yDigitCount < 1) {
+        return x * y;
+    }
+
+    const a = Math.floor(x / Math.pow(10, (xDigitCount / 2)));
+}
+
+function getDigitCount(n) {
+    return n.toString()
+        .split("").length;
 }
 
 module.exports = karatsubaMultiplication;
