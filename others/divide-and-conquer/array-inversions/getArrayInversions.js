f43d50b46e590c6ed846c0c2946882868090a38f
rusev9 <evg.rusev@gmail.com>
2020-04-28 12:09:04 +0300

    implement quadratic brute force solution

--- others/divide-and-conquer/array-inversions/getArrayInversions.js
+++ others/divide-and-conquer/array-inversions/getArrayInversions.js
@@ -1,5 +1,15 @@
 function getArrayInversions(arr) {
-    
+    let numberOfInversions = 0;
+
+    for (let i = arr.length - 1; i >= 1; i--) {
+        for (let j = i - 1; j >= 0; j--) {
+            if (arr[i] < arr[j]) {
+                numberOfInversions++;
+            }
+        }
+    }
+
+    return numberOfInversions;
 }
 
 module.exports = getArrayInversions;
--- others/divide-and-conquer/array-inversions/getArrayInversions.test.js
+++ others/divide-and-conquer/array-inversions/getArrayInversions.test.js
@@ -2,18 +2,18 @@ const getArrayInversions = require("./getArrayInversions");
 
 describe("getArrayInversions", () => {
     it("should return correct number of inversions", () => {
-        expect(getArrayInversions(1, 2, 3, 4)).toBe(0);
+        expect(getArrayInversions([1, 2, 3, 4])).toBe(0);
     });
 
     it("should return correct number of inversions", () => {
-        expect(getArrayInversions(5, 1, 2, 4)).toBe(3);
+        expect(getArrayInversions([5, 1, 2, 4])).toBe(3);
     });
 
     it("should return correct number of inversions", () => {
-        expect(getArrayInversions(0, 3, 7, 9)).toBe(4);
+        expect(getArrayInversions([7, 3, 9, 0])).toBe(4);
     });
 
     it("should return correct number of inversions", () => {
-        expect(getArrayInversions(5, 1, 2, 4, 7, 3, 9, 0)).toBe(13);
+        expect(getArrayInversions([5, 1, 2, 4, 7, 3, 9, 0])).toBe(13);
     });
 });
