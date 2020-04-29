2488af500e99d47b40886ac8f933310f2c268a96
rusev9 <evg.rusev@gmail.com>
2020-04-29 14:01:36 +0300

    add matrix multiplication

---
+++ mathematics/multiplyMatrices/multiplyMatrices.js
@@ -0,0 +1,5 @@
+function multiplyMatrices(m1, m2) {
+    
+}
+
+module.exports = multiplyMatrices;
---
+++ mathematics/multiplyMatrices/multiplyMatrices.test.js
@@ -0,0 +1,22 @@
+const multiplyMatrices = require("./multiplyMatrices");
+
+describe("multiplyMatrices", () => {
+    it("should multiply the matrices correctly", () => {
+        const m1 = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]
+        ];
+        const m2 = [
+            [9, 7, 8],
+            [6, 5, 4],
+            [3, 2, 1]
+        ];
+        
+        expect(multiplyMatrices(m1, m2)).toEqual(
+            [30, 24, 18],
+            [6, 69, 54],
+            [138, 114, 90]
+        );
+    });
+});
