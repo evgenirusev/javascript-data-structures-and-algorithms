e483ebb49f844d75faa8545d1d44adfae058934a
rusev9 <evg.rusev@gmail.com>
2020-04-30 11:46:34 +0300

    add stronger unit tests for each multiplication algorithm

--- mathematics/iterativeMatrixMultiplication/iterativeMatrixMultiplication.js
+++ mathematics/iterativeMatrixMultiplication/iterativeMatrixMultiplication.js
@@ -1,9 +1,11 @@
 function iterativeMatrixMultiplication(m1, m2) {
+    const matrixSize = m1.length;
+
     const result = [...Array(m1.length)].map(e => Array(m1.length).fill(0));
 
-    for (let row = 0; row < m1.length; row++) {
-        for (let col = 0; col < m1[row].length; col++) {
-            for (let i = 0; i < m1[row].length; i++) {
+    for (let row = 0; row < matrixSize; row++) {
+        for (let col = 0; col < matrixSize; col++) {
+            for (let i = 0; i < matrixSize; i++) {
                 result[row][col] += m1[row][i] * m2[i][col];
             }
         }
--- mathematics/iterativeMatrixMultiplication/iterativeMatrixMultiplication.test.js
+++ mathematics/iterativeMatrixMultiplication/iterativeMatrixMultiplication.test.js
@@ -1,7 +1,7 @@
 const iterativeMatrixMultiplication = require("./iterativeMatrixMultiplication");
 
 describe("iterativeMatrixMultiplication", () => {
-    it("should multiply the matrices correctly", () => {
+    it("should multiply the even matrices correctly", () => {
         const m1 = [
             [1, 2],
             [3, 4]
@@ -15,6 +15,51 @@ describe("iterativeMatrixMultiplication", () => {
             [
                 [19, 22],
                 [43, 50]
+            ]
+        );
+    });
+
+    it("should multiply the odd matrices correctly", () => {
+        const m1 = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]
+        ];
+        const m2 = [
+            [9, 7, 8],
+            [6, 5, 4],
+            [3, 2, 1]
+        ];
+
+        expect(iterativeMatrixMultiplication(m1, m2)).toEqual(
+            [
+                [30, 23, 19],
+                [84, 65, 58],
+                [138, 107, 97]
+            ]
+        );
+    });
+
+    it("should multiply the odd matrices correctly", () => {
+        const m1 = [
+            [1, 2, 3, 4],
+            [5, 6, 7, 8],
+            [9, 10, 11, 12],
+            [13, 14, 15, 16]
+        ];
+        const m2 = [
+            [1, 2, 3, 4],
+            [5, 6, 7, 8],
+            [9, 10, 11, 12],
+            [13, 14, 15, 16]
+        ];
+
+        expect(iterativeMatrixMultiplication(m1, m2)).toEqual(
+            [
+                [90, 100, 110, 120],
+                [202, 228, 254, 280],
+                [314, 356, 398, 440],
+                [426, 484, 542, 600]
             ]
         );
     });
