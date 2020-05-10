11c3ca14e94a8af492a1d2fbcbe760b4c8476195
rusev9 <evg.rusev@gmail.com>
2020-05-10 19:13:15 +0300

    implement lomuto left pivot partition subroutene

--- searching/quickSelect/quickSelect.js
+++ searching/quickSelect/quickSelect.js
@@ -10,6 +10,42 @@ function quickSelectRecursive(arr, left, right, kthLargest) {
     if (left >= right) {
         return arr[left];
     }
+
+    const pivot = partition(arr, left, right);
+
+    if (pivot + 1 === kthLargest) {
+        return arr[pivot];
+    }
+
+    if (pivot + 1 < kthLargest) {
+        return quickSelectRecursive(arr, left, pivot - 1);
+    } else {
+        return quickSelectRecursive(arr, pivot + 1, right);
+    }
+}
+
+function partition(arr, left, right) {
+    const pivot = arr[left];
+
+    let i = left + 1;
+    for (let j = i; j <= right; j++) {
+        if (arr[j] < pivot) {
+            if (j !== i) {
+                swap(arr, i, j);
+            }
+
+            i++;
+        }
+    }
+
+    swap(arr, left, i - 1);
+    return i - 1;
+}
+
+function swap(arr, i, j) {
+    const temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
 }
 
 module.exports = quickSelect;
