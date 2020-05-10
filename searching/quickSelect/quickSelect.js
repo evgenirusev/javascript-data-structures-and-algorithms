17535b1fb21a4ba67886cad2866d01bb548d18e7
rusev9 <evg.rusev@gmail.com>
2020-05-10 18:57:42 +0300

    implement base case

--- searching/quickSelect/quickSelect.js
+++ searching/quickSelect/quickSelect.js
@@ -1,5 +1,15 @@
-function quickSelect(arr, element) {
-    
+function quickSelect(arr, kthLargest) {
+    if (kthLargest > arr.length && kthLargest < 0) {
+        throw `invalid argument`;
+    }
+
+    return quickSelectRecursive(arr, 0, arr.length - 1, kthLargest);
 }
 
-module.exports = binarySearch;
+function quickSelectRecursive(arr, left, right, kthLargest) {
+    if (left >= right) {
+        return arr[left];
+    }
+}
+
+module.exports = quickSelect;
