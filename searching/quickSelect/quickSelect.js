d5a7562f1e954a51f73f67ecbb15ceace6efffad
rusev9 <evg.rusev@gmail.com>
2020-05-11 11:51:53 +0300

    rename and move

--- searching/quickSelect/quickSelect.js
+++ searching/quickSelect/quickSelect.js
@@ -1,26 +1,26 @@
-function quickSelect(arr, kthLargest) {
-    if (kthLargest > arr.length && kthLargest < 0) {
+function quickSelect(arr, orderStatistic) {
+    if (orderStatistic > arr.length && orderStatistic < 0) {
         throw `invalid argument`;
     }
 
-    return quickSelectRecursive(arr, 0, arr.length - 1, kthLargest);
+    return quickSelectRecursive(arr, 0, arr.length - 1, orderStatistic);
 }
 
-function quickSelectRecursive(arr, left, right, kthLargest) {
+function quickSelectRecursive(arr, left, right, orderStatistic) {
     if (left >= right) {
         return arr[left];
     }
 
     const pivot = partition(arr, left, right);
 
-    if (pivot + 1 === kthLargest) {
+    if (pivot + 1 === orderStatistic) {
         return arr[pivot];
     }
 
-    if (pivot + 1 > kthLargest) {
-        return quickSelectRecursive(arr, left, pivot - 1, kthLargest);
+    if (pivot + 1 > orderStatistic) {
+        return quickSelectRecursive(arr, left, pivot - 1, orderStatistic);
     } else {
-        return quickSelectRecursive(arr, pivot + 1, right, kthLargest);
+        return quickSelectRecursive(arr, pivot + 1, right, orderStatistic);
     }
 }
 
