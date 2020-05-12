03533e0ace3783e1143707ec72049190d4112fc7
rusev9 <evg.rusev@gmail.com>
2020-05-12 19:46:31 +0300

    update unit test and add implementation

--- graph/adjacencyListGraph/AdjacencyListGraph.js
+++ graph/adjacencyListGraph/AdjacencyListGraph.js
@@ -1,5 +1,24 @@
 class AdjacencyListGraph {
-    
+    constructor(numberOfVertices) {
+        this.vertices = [...Array(numberOfVertices)].map(e => Array());
+    }
+
+    addEdge(vertice1, vertice2) {
+        if (!isVerticeValid(vertice1) && !isVerticeValid(vertice2)) {
+            throw `invalid vertice index - ${vertice}`;
+        }
+
+        this[vertice1].push(vertice2);
+        this[vertice2].push(vertice1);
+    }
+
+    isVerticeValid(vertice) {
+        return isVerticeInBounds(vertice) && typeof vertice === "number";
+    }
+
+    isVerticeInBounds(vertice) {
+        return vertice > 0 && vertice < this.vertices.length;
+    }
 }
 
-module.exports = adjacencyListGraph;
+module.exports = AdjacencyListGraph;
--- graph/adjacencyListGraph/AdjacencyListGraph.test.js
+++ graph/adjacencyListGraph/AdjacencyListGraph.test.js
@@ -3,15 +3,17 @@ const AdjacencyListGraph = require("./AdjacencyListGraph");
 describe("AdjacencyListGraph", () => {
     let adjacencyListGraph;
 
-    beforeEach(() => {
-        adjacencyListGraph = new AdjacencyListGraph();
+    it("should add edges and retrive the correct amout of vertices", () => {
+        adjacencyListGraph = new AdjacencyListGraph(5);
+        expect(adjacencyListGraph.getNumerOfVertices()).toBe(5);
     });
 
     it("should add edges and retrive the correct amout of vertices", () => {
-        adjacencyListGraph.addVertice(1);
-        adjacencyListGraph.addVertice(2);
-        adjacencyListGraph.addVertice(3);
-
-        expect(adjacencyListGraph.getNumerOfVertices()).toBe(3);
+        adjacencyListGraph = new AdjacencyListGraph(5);
+        adjacencyListGraph.addEdge(0, 1);
+        adjacencyListGraph.addEdge(0, 2);
+        adjacencyListGraph.addEdge(1, 2);
+        adjacencyListGraph.addEdge(1, 3);
+        adjacencyListGraph.addEdge(3, 1);
     });
 });
