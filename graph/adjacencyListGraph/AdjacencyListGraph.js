d34cfa12b55c84b443e2c8f32a9dba37d023a1a8
rusev9 <evg.rusev@gmail.com>
2020-05-12 21:04:25 +0300

    implement degree method

--- graph/adjacencyListGraph/AdjacencyListGraph.js
+++ graph/adjacencyListGraph/AdjacencyListGraph.js
@@ -8,24 +8,28 @@ class AdjacencyListGraph {
     }
 
     addEdge(vertice1, vertice2) {
-        if (!this._isVerticeValid(vertice1) || !this._isVerticeValid(vertice2)) {
-            throw `invalid vertices`;
-        }
+        this._validateVertice(vertice1);
+        this._validateVertice(vertice2);
 
         this._vertices[vertice1].add(vertice2);
         this._vertices[vertice2].add(vertice1);
     }
 
     getEdges(vertice) {
-        if (typeof this._vertices[vertice] === "undefined") {
-            throw `invalid vertice - ${vertice}`;
-        }
-
+        this._validateVertice(vertice);
         return [...this._vertices[vertice]];
     }
 
-    _isVerticeValid(vertice) {
-        return this._isVerticeInBounds(vertice) && typeof vertice === "number";
+    degree(vertice) {
+        this._validateVertice(vertice);
+
+        return [...this._vertices[vertice]].length;
+    }
+
+    _validateVertice(vertice) {
+        if (!this._isVerticeInBounds(vertice) && typeof vertice !== "number") {
+            throw `invalid vertice ${vertice}`;
+        }
     }
 
     _isVerticeInBounds(vertice) {
