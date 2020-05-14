76a48702de1f8beedac41dea342d2bd550b9ea7d
rusev9 <evg.rusev@gmail.com>
2020-05-14 17:56:11 +0300

    implement adjacency list graph - getEdges and degree

--- src/data-structures/graphs/undirected/adjacency-matrix-graph/AdjacencyMatrixGraph.js
+++ src/data-structures/graphs/undirected/adjacency-matrix-graph/AdjacencyMatrixGraph.js
@@ -1,22 +1,46 @@
 class AdjacencyMatrixGraph {
     constructor(numberOfVertices) {
-        this.graph = [...Array(numberOfVertices)].map(e => Array(numberOfVertices));
+        this._graph = [...Array(numberOfVertices)].map(e => Array(numberOfVertices));
     }
 
     getNumerOfVertices() {
-        return this.graph.length;
+        return this._graph.length;
     }
 
     addEdge(vertice1, vertice2) {
-        
+        this._validateVertice(vertice1);
+        this._validateVertice(vertice2);
+
+        this._graph[vertice1][vertice2] = true;
+        this._graph[vertice2][vertice1] = true;
     }
 
     getEdges(vertice) {
-        // todo
+        this._validateVertice(vertice);
+
+        let edges = [];
+        this._graph[vertice].forEach((edgeExists, index) => {
+            if (edgeExists) {
+                edges.push(index);
+            }
+        });
+
+        return edges;
     }
 
     degree(vertice) {
-        // todo
+        this._validateVertice(vertice);
+        return this.getEdges(vertice).length;
+    }
+
+    _validateVertice(vertice) {
+        if (!this._isVerticeInBounds(vertice) && typeof vertice !== "number") {
+            throw `invalid vertice ${vertice}`;
+        }
+    }
+
+    _isVerticeInBounds(vertice) {
+        return vertice >= 0 && vertice < this._graph.length;
     }
 }
 
