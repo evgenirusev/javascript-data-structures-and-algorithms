66c9bb44d04cbc25d2e15d9057ffff4d118100ba
evgenirusev <evgeni.n.rusev@gmail.com>
2020-07-24 23:37:04 +0300

    initial implementation

--- src/sorting/shell-sort/shellSort.js
+++ src/sorting/shell-sort/shellSort.js
@@ -1,5 +1,16 @@
 function shellSort(array, cmp) {
-    // todo: implement me
+    if (typeof cmp !== "function") {
+        cmp = (a, b) => a - b;
+    }
+
+    for (let interval = array.length / 2; interval > 0; interval /= 2) {
+        for (let right = interval; right < array.length; right++) {
+            let left = right - interval;
+            if (left < interval && array[right] < array[left]) {
+                swap(array, left, right);
+            }
+        }
+    }
 }
 
 function swap(array, left, right) {
@@ -9,4 +20,10 @@ function swap(array, left, right) {
     array[right] = temp;
 }
 
-module.exports = shellSort;
+let arr = [9, 8, 3, 7, 5, 6, 4, 1];
+
+shellSort(arr);
+
+arr.forEach(x => {
+    console.log(x);
+});
