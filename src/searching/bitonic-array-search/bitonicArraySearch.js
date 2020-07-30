485b944009fc32dea06e9410bbec739825edbb8b
evgenirusev <evgeni.n.rusev@gmail.com>
2020-07-30 19:07:49 +0300

    implement utils

--- src/searching/bitonic-array-search/bitonicArraySearch.js
+++ src/searching/bitonic-array-search/bitonicArraySearch.js
@@ -1,5 +1,23 @@
-function bitonicArraySearch() {
-    // todo
+function bitonicArraySearch(array, n) {
+    let mid = Math.floor(0, array.length - 1);
+
+    while (!isDescendingSequenceBoundry(array, mid)) {
+    }
+}
+
+function isIndexAtBoundry(array, index) {
+    return array[index] < array[index - 1]
+        && array[index] > array[index + 1];
+}
+
+function isBoundryLeftSide(array, index) {
+    return array[index] < array[index + 1]
+        array[index] > array[index - 1];
+}
+
+function isBoundryRightSide(array, index) {
+    return array[index] < array[index + 1]
+        array[index] > array[index - 1];
 }
 
 module.exports = bitonicArraySearch;
