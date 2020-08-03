86d9f78e6fc19d0b66bfb79838e70e383f4b31fa
evgenirusev <evgeni.n.rusev@gmail.com>
2020-08-03 15:57:37 +0300

    implement inefficient O(n) space & theta O(2N) time

--- src/others/rotate-array/rotateArray.js
+++ src/others/rotate-array/rotateArray.js
@@ -1,5 +1,16 @@
-function rotateArray(array, rotations) {
-    // implement
+function rotateArray(array, rotationsCount) { 
+    let clonedArray = [...array]; 
+    for (let i = 0; i < array.length; i++) {
+        let rotatedValueIndex = i - rotationsCount;
+
+        if (rotatedValueIndex < 0) {
+            rotatedValueIndex = array.length + rotatedValueIndex;
+        }
+
+        array[i] = clonedArray[rotatedValueIndex];
+    }
+
+    return array;
 }
 
 module.exports = rotateArray;
--- src/others/rotate-array/rotateArray.test.js
+++ src/others/rotate-array/rotateArray.test.js
@@ -2,7 +2,7 @@ const rotateArray = require("./rotateArray");
 
 describe("rotateArray", () => {
     it("should rotate the array once", () => {
-        expect(rotateArray([1, 2, 3, 4, 5, 6]))
-            .toBe([6,1,2,3,4,5]);
+        expect(rotateArray([1, 2, 3, 4, 5, 6], 1))
+            .toEqual([6,1,2,3,4,5]);
     });
 });
