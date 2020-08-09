e805d08bef67f8f6ac062418087a8c79eb4a6d08
evgenirusev <evgeni.n.rusev@gmail.com>
2020-08-09 11:02:37 +0300

    implement point data structure

--- src/data-structures/mathematics/Point/Point.js
+++ src/data-structures/mathematics/Point/Point.js
@@ -1,4 +1,12 @@
 class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    compareTo(point) {
+        return point.x === this.x && point.y === this.y ? 1 : -1;
+    }
 }
 
 module.exports = Point;
