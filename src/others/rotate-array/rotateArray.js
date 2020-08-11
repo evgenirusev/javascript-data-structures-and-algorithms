a539cf30f1c389b11b4b4a54975552b4a0717c46
evgenirusev <evgeni.n.rusev@gmail.com>
2020-08-11 22:48:35 +0300

    implement working rotate array

--- src/others/rotate-array/rotateArray.js
+++ src/others/rotate-array/rotateArray.js
@@ -1,22 +1,13 @@
 function rotateArray(array, rotationsCount) { 
-    let tempElements = [];
     for (let i = 0; i < rotationsCount; i++) {
-        tempElements.push(array[i]);
-    }
+        const lastIndex = array.length - 1;
+        const lastValue = array[lastIndex];
 
-    const lastIndex = array.length - 1;
-    for (let i = lastIndex; i >= rotationsCount; i--) {
-        let currentElementIndex = i + rotationsCount;
-
-        if (currentElementIndex > lastIndex) {
-            currentElementIndex -= lastIndex;
+        for (let j = lastIndex; j > 0; j--) {
+            array[j] = array[j - 1];
         }
 
-        array[i] = array[currentElementIndex];
-    }
-
-    for (let i = array.length - rotationsCount; i < array.length; i++) {
-        array[i] = tempElements[i];
+        array[0] = lastValue;
     }
 
     return array;
