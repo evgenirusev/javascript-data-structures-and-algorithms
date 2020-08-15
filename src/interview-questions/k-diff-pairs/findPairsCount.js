c311731edf6fcac8b1e9574a2d7d3986c38ff2fd
evgenirusev <evgeni.n.rusev@gmail.com>
2020-08-15 16:35:00 +0300

    optimize solution

--- src/interview-questions/k-diff-pairs/findPairsCount.js
+++ src/interview-questions/k-diff-pairs/findPairsCount.js
@@ -2,23 +2,18 @@
 // you need to find the number of unique k-diff pairs in the array.
 
 function findPairsCount(array, k) {
+    const map = {};
+    array.forEach(x => {
+        map[x] = true;
+    });
+
     let count = 0;
-    let pairs = {};
-
-    for (let i = 0; i < array.length; i++) {
-        for (let j = 0; j < array.length; j++) {
-            const val1 = array[i];
-            const val2 = array[j];
-
-            if (i !== j 
-                    && Math.abs(val1 - val2) === k 
-                    && !pairs[`${val1}${val2}`] 
-                    && !pairs[`${val2}${val1}`]) {
-                count++;
-                pairs[`${val1}${val2}`] = true;
-            }
+    array.forEach(x => {
+        const leftOver = x - k;
+        if (map[leftOver]) {
+            count++;
         }
-    }
+    });
 
     return count;
 }
