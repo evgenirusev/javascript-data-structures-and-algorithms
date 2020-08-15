9e44bf599871c363b0059fe7f7685769ae181ba0
evgenirusev <evgeni.n.rusev@gmail.com>
2020-08-15 18:30:05 +0300

    implement javascript helpers solution

--- src/interview-questions/reverse-words-in-string/reverseWordsInString.js
+++ src/interview-questions/reverse-words-in-string/reverseWordsInString.js
@@ -1,25 +1,9 @@
 function reverseWordsInString(str) {
-    // return str
-            // .split(/\s+/)
-            // .reverse()
-            // .join(" ");
-
-    let result = "";
-    let currentWord = "";
-    for (let i = str.length - 1; i >= 0; i--) {
-        const currentChar = str.charAt(i);
-
-        if (currentChar !== " ") {
-            currentWord = currentChar + currentWord;
-        } else {
-            if (currentWord !== "") {
-                result += `${currentWord} `;
-                currentWord = "";
-            }
-        }
-    }
-
-    return (result + currentWord).trim();
+    return str
+            .split(/\s+/)
+            .reverse()
+            .join(" ")
+            .trim();
 }
 
 module.exports = reverseWordsInString;
