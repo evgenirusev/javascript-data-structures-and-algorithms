e315df8378d3252d9fd8db049cf40e31fe71b4f0
evgenirusev <evgeni.n.rusev@gmail.com>
2020-08-17 21:53:40 +0300

    implement stack solution

--- src/mathematics/reverse-polish-notation/evaluateRPN.js
+++ src/mathematics/reverse-polish-notation/evaluateRPN.js
@@ -1,5 +1,28 @@
+const operands = {
+    "+": (a, b) => a + b,
+    "-": (a, b) => b - a,
+    "/": (a, b) => b / a,
+    "*": (a, b) => a * b
+}
+
 function evaluateRPN(operations) {
-     
+    const stack = [];
+    operations.forEach(token => {
+        if (operands[token]) {
+            stack.push(
+                operands[token](
+                    stack.pop(),
+                    stack.pop()
+                )
+            );
+        } else {
+            stack.push(parseInt(token));
+        }
+    });
+
+    return Math.floor(
+        stack.pop()
+    );
 }
 
 module.exports = evaluateRPN;
