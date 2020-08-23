14889918aa4c3eb551f3846ed5381e02a913d01d
evgenirusev <evgeni.n.rusev@gmail.com>
2020-08-23 21:20:20 +0300

    Update

---
+++ src/interview-questions/wildcard-matching/isMatch.js
@@ -0,0 +1,8 @@
+// Given an input string (s) and a pattern (p), implement wildcard pattern 
+// matching with support for '?' and '*'.
+
+function isMatch(str, pattern) {
+
+}
+
+module.exports = isMatch
---
+++ src/interview-questions/wildcard-matching/isMatch.test.js
@@ -0,0 +1,23 @@
+const isMatch = require('./isMatch')
+
+describe('isMatch', () => { 
+   it("should't match", () => {
+       expect(isMatch("aa", "a")).toBe(false);
+   }); 
+
+   it('should match', () => {
+       expect(isMatch("aa", "*")).toBe(true);
+   }); 
+
+   it("shouldn't match", () => {
+       expect(isMatch("cb", "?a")).toBe(false);
+   }); 
+
+   it('should match', () => {
+       expect(isMatch("adceb", "*a*b")).toBe(true);
+   }); 
+
+   it("shouldn't match", () => {
+       expect(isMatch("acdcb", "*a*b")).toBe(false);
+   }); 
+});
