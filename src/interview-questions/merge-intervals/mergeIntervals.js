691a737e5b80feab22642525160ff87c14063103
evgenirusev <evgeni.n.rusev@gmail.com>
2020-08-28 00:16:07 +0300

    Implement mergeIntervals

--- src/interview-questions/merge-intervals/mergeIntervals.js
+++ src/interview-questions/merge-intervals/mergeIntervals.js
@@ -1,5 +1,30 @@
-function mergeIntervals() {
-   // TODO: implement
+function mergeIntervals(intervals) {
+    let intervalIndex = 0;
+    while (intervalIndex < intervals.length - 1) {
+        const interval1 = intervals[intervalIndex];
+        const interval2 = intervals[intervalIndex + 1];
+
+        if (areOverlapping(interval1, interval2)) {
+            mergeIntoInterval2(interval1, interval2);
+        }
+    }
+
+    return intervals.filter(interval => interval);
 }
 
-module.exports = mergeIntervals
+function areOverlapping(interval1, interval2) {
+    return interval1[1] >= interval2[0] 
+        || interval1[0] === interval[0];
+}
+
+function mergeIntoInterval2(intervals, interval1Index, interval2Index) {
+    const interval1 = intervals[interval1Index];
+    const interval2 = intervals[interval2Index];
+
+    interval2[0] = interval1[0];
+    interval2[1] = interval1[1] < interval2[1] ? interval2[1] : interval2[1];
+
+    intervals[interval1Index] = null;
+}
+
+module.exports = mergeIntervals;
