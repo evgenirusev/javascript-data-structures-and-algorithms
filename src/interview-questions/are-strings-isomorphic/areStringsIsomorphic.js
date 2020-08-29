ac778cd5933dbb84e3e5eccac31cd3cb32ab88e0
evgenirusev <evgeni.n.rusev@gmail.com>
2020-08-29 13:36:15 +0300

    Update

--- src/interview-questions/are-strings-isomorphic/areStringsIsomorphic.js
+++ src/interview-questions/are-strings-isomorphic/areStringsIsomorphic.js
@@ -2,18 +2,28 @@
 // Assume strings are of equal lengths
 
 function areStringsIsomorphic(str1, str2) {
-    const charsMap = {};
+    const str1ToStr2Chars = {};
+    const str2ToStr1Chars = {};
 
     for (let i = 0; i < str1.length; i++) {
         const char1 = str1.charAt(i);
         const char2 = str2.charAt(i);
+        const isStr1CharMatch = typeof str1ToStr2Chars[char1] !== "undefined";
+        const isStr2CharMatch = typeof str2ToStr1Chars[char2] !== "undefined";
 
-        if (typeof charsMap[char1] === "undefined") {
-            charsMap[char1] = char2;
-        } else {
-            if (charsMap[char1] !== char2) {
-                return false;
+        if (isStr1CharMatch || isStr2CharMatch) {
+            if (isStr1CharMatch) {
+                if (str1ToStr2Chars[char1] !== char2) {
+                    return false;
+                }
+            } else {
+                if (str2ToStr1Chars[char2] !== char1) {
+                    return false;
+                }
             }
+        } else {
+            str1ToStr2Chars[char1] = char2;
+            str2ToStr1Chars[char2] = char1;
         }
     }
 
