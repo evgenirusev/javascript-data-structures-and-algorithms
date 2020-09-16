005204132787cc0f81228334a08c2c4cc0b8bc98
evgenirusev <evgeni.n.rusev@gmail.com>
2020-09-16 22:55:03 +0300

    add extra unit tests

--- src/graph-algorithms/shortest-path/unweighted/bfs-shortest-path/bfsShortestPath.test.js
+++ src/graph-algorithms/shortest-path/unweighted/bfs-shortest-path/bfsShortestPath.test.js
@@ -6,5 +6,8 @@ describe('bfsShortestPath', () => {
        const graph = graphMockGenerators.generateUnweightedGraph();
 
        expect(bfsShortestPath(graph, graph.getVertex("k1"), graph.getVertex("k5"))).toBe(2);
+       expect(bfsShortestPath(graph, graph.getVertex("k1"), graph.getVertex("k9"))).toBe(2);
+       expect(bfsShortestPath(graph, graph.getVertex("k1"), graph.getVertex("k7"))).toBe(2);
+       expect(bfsShortestPath(graph, graph.getVertex("k1"), graph.getVertex("k8"))).toBe(3);
    }); 
 });
