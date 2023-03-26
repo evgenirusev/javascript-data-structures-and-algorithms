d2a3f191d0f46c973552a6336270af69470f4145
Evgeni Rusev <evgeni.n.rusev@gmail.com>
2023-03-26 20:36:19 +0300

    implement dijkstra shortest path

--- src/graph-algorithms/shortest-path/dijkstra/dijkstra.js
+++ src/graph-algorithms/shortest-path/dijkstra/dijkstra.js
@@ -1,9 +1,34 @@
 const BinaryHeap = require('../../../data-structures/heaps/binary-heap/BinaryHeap');
 
 function getShortestPaths(graph, sourceVertex) {
+    const visited = {};
+    const distances = {};
+    Object.keys(graph.allVertices).forEach(vertex => {
+        distances[vertex] = Infinity;
+    });
     const heap = new BinaryHeap((a, b) => b.distance - a.distance);
-    heap.insert();
-    while (heap)
+
+    heap.insert({ vertex: sourceVertex, distance: 0 });
+    while (!heap.isEmpty) {
+        const currentVertex = heap.extract();
+        visited[currentVertex] = true;
+
+        graph.outgoingEdgesOf(currentVertex).forEach(edge => {
+            if (!visited[edge.end]) {
+                const distanceWithEdge = distances[currentVertex] + edge.weight;
+                const endVertexDistance = distances[edge.end];
+                
+                if (distanceWithEdge < endVertexDistance) {
+                    distances[edge.end] = distanceWithEdge;
+
+                    heap.insert({
+                        vertex: graph.getVertex(edge.end),
+                        distance: distances[edge.end]
+                    });
+                }
+            }
+        });
+    }
 }
 
-module.exports = breadthFirstSearch;
+module.exports = getShortestPaths;
