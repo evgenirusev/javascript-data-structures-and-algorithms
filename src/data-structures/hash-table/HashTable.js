4a616109229e7df841fc12b155105313df933647
Evgeni Rusev <evgeni.n.rusev@gmail.com>
2023-04-02 21:54:16 +0300

    Add unit tests for remove, put and get

--- src/data-structures/hash-table/HashTable.js
+++ src/data-structures/hash-table/HashTable.js
@@ -1,5 +1,14 @@
 class HashTable {
-    
+    constructor() {
+      this.table = [];
+      this.size = 0;
+    }
+
+    add(key, value) { }
+
+    get(key) { }
+
+    remove(key) {}
 }
 
 module.exports = HashTable;
--- src/data-structures/hash-table/HashTable.test.js
+++ src/data-structures/hash-table/HashTable.test.js
@@ -1,3 +1,27 @@
+const HashTable = require('./HashTable');
+
 describe("HashTable", () => {
-    
+    let hashTable;
+
+    beforeEach(() => {
+        hashTable = new HashTable();
+    });
+
+    it('should put and get correct values', () => {
+        hashTable.put('key1', 1);
+        hashTable.put('key2', 2);
+
+        expect(hashTable.get('key1')).toBe(1);
+        expect(hashTable.get('key2')).toBe(2);
+    });
+
+    test('remove', () => {
+        hashTable.put('key1', 1);
+        hashTable.put('key2', 2);
+
+        hashTable.remove('key2');
+
+        expect(hashTable.get('key1')).toBeUndefined();
+        expect(hashTable.get('key2')).toBe(2);
+    });
 });
