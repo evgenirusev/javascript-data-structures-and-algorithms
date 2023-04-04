d57419ab3fdfe8fb8bba052e839c5e2443bb3a9a
Evgeni Rusev <evgeni.n.rusev@gmail.com>
2023-04-04 23:31:24 +0300

    Add unit test for prim's algorithm

--- src/graph-algorithms/spanning-trees/prim/prim.test.js
+++ src/graph-algorithms/spanning-trees/prim/prim.test.js
@@ -1,7 +1,12 @@
 const primFindMinimumWeightTree = require('./primFindMinimumWeightTree');
+const graphGenerator = require('../../../data-structures/graphs/graph-mock-generator');
+const Edge = require('../../../data-structures/graphs/edge/Edge.js');
 
 describe('primFindMinimumWeightTree', () => { 
-    it('', () => {
-        
+    it('should return the minimum weight tree', () => {
+        const graph = graphGenerator.generateWeightedGraph();
+
+        expect(primFindMinimumWeightTree(graph))
+            .toEqual(["v1", "v3", "v5", "v9", "v7", "v8", "v2", "v4", "v12", "v6", "v10", "v11"]);
     }); 
 });
