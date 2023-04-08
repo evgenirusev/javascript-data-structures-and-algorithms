0f8d75acce5237ad0e06ac54bf9c416993c016b5
Evgeni Rusev <evgeni.n.rusev@gmail.com>
2023-04-08 18:30:19 +0300

    add unit test

--- src/data-structures/disjoint-sets/union-find/UnionFind.test.js
+++ src/data-structures/disjoint-sets/union-find/UnionFind.test.js
@@ -7,12 +7,31 @@ describe('UnionFind', () => {
         unionFind = new UnionFind(["k1","k3","k5","k2","k4","k12","k8","k9","k7","k6"]);
     });
 
-    it('should find the correct root element', () => {
+    it('should find the correct root elements after a union', () => {
         expect(unionFind.findRoot("k1")).toBe("k1");
 
         unionFind.union("k2", "k4");
 
         expect(unionFind.findRoot("k2")).toBe("k2");
         expect(unionFind.findRoot("k4")).toBe("k2");
+
+        unionFind.union("k1", "k2");
+        expect(unionFind.findRoot("k1")).toBe("k2");
+    });
+
+    it('should find k2 to be the root node in the union', () => {
+        unionFind.union("k6", "k7");
+        unionFind.union("k2", "k4");
+        unionFind.union("k8", "k9");
+        unionFind.union("k4", "k8");
+        unionFind.union("k2", "k3");
+        unionFind.union("k7", "k8");
+
+        expect(unionFind.findRoot("k6")).toBe("k2");
+        expect(unionFind.findRoot("k2")).toBe("k2");
+        expect(unionFind.findRoot("k8")).toBe("k2");
+        expect(unionFind.findRoot("k4")).toBe("k2");
+        expect(unionFind.findRoot("k2")).toBe("k2");
+        expect(unionFind.findRoot("k7")).toBe("k2");
     });
 });
