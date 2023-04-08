0635d48a28fea75510f611861de2736feba66fef
Evgeni Rusev <evgeni.n.rusev@gmail.com>
2023-04-08 18:19:59 +0300

    Implemeent union find and refactor unit tests

--- src/data-structures/disjoint-sets/union-find/UnionFind.js
+++ src/data-structures/disjoint-sets/union-find/UnionFind.js
@@ -1,7 +1,37 @@
 class UnionFind {
-    constructor(size) { }
-    find(key) { }
-    union(key1, key2) { }
+    constructor(keys) {
+        this.parent = {};
+        this.rank = {};
+
+        keys.forEach(key => {
+            this.parent[key] = key;
+            this.rank[key] = 0;
+        });
+    }
+    
+    findRoot(key) {
+        if (this.parent[key] !== key) {
+            this.parent[key] = this.findRoot(this.parent[key]);
+        }
+
+        return this.parent[key];
+    }
+    
+    union(key1, key2) {
+        let rootKey1 = this.findRoot(key1);
+        let rootKey2 = this.findRoot(key2);
+
+        if (rootKey1 === rootKey2) {
+          return;
+        }
+        if (this.rank[rootKey1] < this.rank[rootKey2]) {
+          [rootKey1, rootKey2] = [rootKey2, rootKey1];
+        }
+        this.parent[rootKey2] = rootKey1;
+        if (this.rank[rootKey1] === this.rank[rootKey2]) {
+          this.rank[rootKey1]++;
+        }
+    }
 }
 
 module.exports = UnionFind;
--- src/data-structures/disjoint-sets/union-find/UnionFind.test.js
+++ src/data-structures/disjoint-sets/union-find/UnionFind.test.js
@@ -4,18 +4,15 @@ describe('UnionFind', () => {
     let unionFind;
     
     beforeEach(() => {
-        unionFind = new UnionFind(5);
+        unionFind = new UnionFind(["k1","k3","k5","k2","k4","k12","k8","k9","k7","k6"]);
     });
 
     it('should find the correct root element', () => {
-        unionFind.union(0, 1);
-        unionFind.union(2, 3);
-        unionFind.union(1, 2);
-        
-        expect(unionFind.find(0)).toBe(3);
-        expect(unionFind.find(1)).toBe(3);
-        expect(unionFind.find(2)).toBe(3);
-        expect(unionFind.find(3)).toBe(3);
-        expect(unionFind.find(4)).toBe(4);
+        expect(unionFind.findRoot("k1")).toBe("k1");
+
+        unionFind.union("k2", "k4");
+
+        expect(unionFind.findRoot("k2")).toBe("k2");
+        expect(unionFind.findRoot("k4")).toBe("k2");
     });
 });
