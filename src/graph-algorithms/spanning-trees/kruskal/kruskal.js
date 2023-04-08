827ce75d6c485edd4a54e2e10c7a521b9b64c335
Evgeni Rusev <evgeni.n.rusev@gmail.com>
2023-04-08 20:13:00 +0300

    Kruskal's algorithm implementation

--- src/graph-algorithms/spanning-trees/kruskal/kruskal.js
+++ src/graph-algorithms/spanning-trees/kruskal/kruskal.js
@@ -1,7 +1,19 @@
 const UnionFind = require('../../../data-structures/disjoint-sets/union-find/UnionFind');
 
 function kruskalFindMinimumWeightTree(edges, keys) {
+    edges.sort((a, b) => a.weight - b.weight);
+    const unionFind = new UnionFind(keys);
     
+    const minimumWeightTree = [];
+    edges.forEach(edge => {
+        if (unionFind.findRoot(edge.start) !== unionFind.findRoot(edge.end)) {
+            unionFind.union(edge.start, edge.end);
+
+            minimumWeightTree.push(edge.end);
+        }
+    });
+
+    return minimumWeightTree;
 }
 
 module.exports = kruskalFindMinimumWeightTree;
