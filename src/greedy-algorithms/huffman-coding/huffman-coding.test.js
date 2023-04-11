985a0e00e7d541c96d11cc3d6e86e354dc8b37ed
Evgeni Rusev <evgeni.n.rusev@gmail.com>
2023-04-11 22:10:36 +0300

    add huffman-coding

---
+++ src/greedy-algorithms/huffman-coding/huffman-coding.test.js
@@ -0,0 +1,11 @@
+const { HuffmanCoding } = require("./huffman-coding");
+
+describe("HuffmanCoding", () => {
+  it("should encode and decode successfully", () => {
+    const huffmanCoding = new HuffmanCoding();
+    const input = "Test string abcacdadc";
+    const { encodedText, huffmanTree } = huffmanCoding.encode(input);
+
+    expect(huffmanCoding.decode(encodedText, huffmanTree)).toEqual(input);
+  });
+});
