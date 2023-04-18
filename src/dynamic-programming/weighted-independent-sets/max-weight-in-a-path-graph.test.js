ca00065e86bce62ea8f9194a336463e481af8fc3
Evgeni Rusev <evgeni.n.rusev@gmail.com>
2023-04-18 22:51:06 +0300

    fix unit tests

--- src/dynamic-programming/weighted-independent-sets/max-weight-in-a-path-graph.test.js
+++ src/dynamic-programming/weighted-independent-sets/max-weight-in-a-path-graph.test.js
@@ -5,8 +5,8 @@ describe("maxWeightInAPathGraph", () => {
     const pathGraph = [[1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7], [7, 8], [8, 9], [9, 10]];
     const weights = [2, 5, 1, 10, 4, 7, 3, 6, 8, 4];
 
-    // Graph  -  1---2---3---4---5---6---7---8---9---10
+    // Graph   - 1---2---3---4---5---6---7---8---9---10
     // Weights - 2---5---1---10---4---7---3---6---8---4
-    expect(maxWeightInAPathGraph(pathGraph, weights)).ToBe(35);
+    expect(maxWeightInAPathGraph(pathGraph, weights)).toBe(30);
   });
 });
