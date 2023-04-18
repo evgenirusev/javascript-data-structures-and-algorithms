b0a25b58c7ff1fd06ae7d7cdfe4f48fab82786b7
Evgeni Rusev <evgeni.n.rusev@gmail.com>
2023-04-18 22:37:41 +0300

    implementation

--- src/dynamic-programming/weighted-independent-sets/max-weight-in-a-path-graph.js
+++ src/dynamic-programming/weighted-independent-sets/max-weight-in-a-path-graph.js
@@ -1,5 +1,16 @@
 function maxWeightInAPathGraph(pathGraph, weights) {
-  
+    const numVertices = pathGraph.length;
+    const maxWeightIs = new Array(numVertices + 1).fill(0);
+    maxWeightIs[1] = weights[0];
+
+    for (let i = 2; i <= numVertices; i++) {
+        maxWeightIs[i] = Math.max(
+            maxWeightIs[i - 1],
+            maxWeightIs[i - 2] + weights[i - 1]
+        );
+    }
+
+    return maxWeightIs[numVertices];
 }
 
 module.exports = maxWeightInAPathGraph;
--- src/dynamic-programming/weighted-independent-sets/max-weight-in-a-path-graph.test.js
+++ src/dynamic-programming/weighted-independent-sets/max-weight-in-a-path-graph.test.js
@@ -4,6 +4,9 @@ describe("maxWeightInAPathGraph", () => {
   it("should return the max weight for the path graph", () => {
     const pathGraph = [[1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7], [7, 8], [8, 9], [9, 10]];
     const weights = [2, 5, 1, 10, 4, 7, 3, 6, 8, 4];
+
+    // Graph  -  1---2---3---4---5---6---7---8---9---10
+    // Weights - 2---5---1---10---4---7---3---6---8---4
     expect(maxWeightInAPathGraph(pathGraph, weights)).ToBe(35);
   });
 });
