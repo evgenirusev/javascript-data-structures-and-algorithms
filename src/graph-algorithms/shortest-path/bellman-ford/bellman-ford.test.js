c7d3efaad584aea0d34e05ce2d1254ebe27c4c67
Evgeni Rusev <evgeni.n.rusev@gmail.com>
2023-04-23 15:24:11 +0300

    bellman ford - cycle unit test

--- src/graph-algorithms/shortest-path/bellman-ford/bellman-ford.test.js
+++ src/graph-algorithms/shortest-path/bellman-ford/bellman-ford.test.js
@@ -37,4 +37,47 @@ describe('bellmanFord', () => {
             'F': 3
         });
     });
+
+    it('should throw an error when a negative weight cycle is present', () => {
+        const graph = new Graph();
+        ["A", "B", "C", "D", "E"].forEach(vertexKey => {
+          graph.addVertex(new Vertex(vertexKey));
+        });
+      
+        [
+          new Edge('A', 'B', 6),
+          new Edge('A', 'C', 4),
+          new Edge('B', 'C', -2),  // negative cycle
+          new Edge('C', 'D', 1),
+          new Edge('D', 'C', -5),  // negative cycle
+          new Edge('D', 'E', 3)
+        ].forEach(edge => {
+          graph.addEdge(edge);
+        });
+      
+        expect(() => bellmanFord(graph, 'A')).toThrowError('Negative weight cycle detected');
+      });
+      
+    it("should return the correct shortest path", () => {
+        const graph = new Graph();
+        ["A", "B", "C", "D"].forEach(vertexKey => {
+            graph.addVertex(new Vertex(vertexKey));
+        });
+
+        [
+            new Edge('A', 'B', 4),
+            new Edge('A', 'C', 3),
+
+            new Edge('B', 'D', 7),
+
+            new Edge('C', 'B', 7),
+            
+            new Edge('D', 'C', -15)
+        ].forEach(edge => {
+            graph.addEdge(edge);
+        });
+
+        expect(() => bellmanFord(graph, 'A'))
+            .toThrowError('Negative cycle detected');
+    });
 });
