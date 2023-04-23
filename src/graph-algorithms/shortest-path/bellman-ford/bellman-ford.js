a5de958408e3875fbd0f2ee4e99080945f731d86
Evgeni Rusev <evgeni.n.rusev@gmail.com>
2023-04-23 15:28:34 +0300

    bellman ford - increase abstraction

--- src/graph-algorithms/shortest-path/bellman-ford/bellman-ford.js
+++ src/graph-algorithms/shortest-path/bellman-ford/bellman-ford.js
@@ -7,19 +7,23 @@ function bellmanFord(graph, sourceVertex) {
     distances[sourceVertex] = 0;
 
     for (let i = 0; i < graph.allVertices.length - 1; i++) {
-        graph.allVertices.forEach(vertex => {
-            graph.outgoingEdgesOf(vertex).forEach(edge => {
-                const totalDistance = edge.weight + distances[vertex];
-                if (totalDistance < distances[edge.end]) {
-                    distances[edge.end] = totalDistance
-                }
-            });
-        });
+        relaxVertices(graph, distances);
     }
 
     checkForNegativeSycle(graph, distances);
 
     return distances;
+}
+
+function relaxVertices(graph, distances) {
+    graph.allVertices.forEach(vertex => {
+        graph.outgoingEdgesOf(vertex).forEach(edge => {
+            const totalDistance = edge.weight + distances[vertex];
+            if (totalDistance < distances[edge.end]) {
+                distances[edge.end] = totalDistance
+            }
+        });
+    });
 }
 
 function checkForNegativeSycle(graph, distances) {
