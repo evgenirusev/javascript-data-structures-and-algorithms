0d44bc3bac21a36e0f605fae40f79be6d4307874
Evgeni Rusev <evgeni.n.rusev@gmail.com>
2023-04-23 21:08:00 +0300

    floyd-warshall - Code quality improvement

--- src/graph-algorithms/shortest-path/floyd-warshall/floyd-warshall.js
+++ src/graph-algorithms/shortest-path/floyd-warshall/floyd-warshall.js
@@ -1,5 +1,31 @@
 function floydWarshall(graph) {
-    return [];
+    const verticesCount = graph.getNumerOfVertices();
+  
+    const dist = initializeDistanceMatrix(graph, verticesCount);
+
+    for (let k = 0; k < verticesCount; k++) {
+        for (let i = 0; i < verticesCount; i++) {
+            for (let j = 0; j < verticesCount; j++) {
+                if (dist[i][j] > dist[i][k] + dist[k][j]) {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+
+    return dist;
+}
+
+function initializeDistanceMatrix(graph, verticesCount) {
+    new Array(verticesCount)
+        .fill()
+        .map(() => new Array(verticesCount).fill(Infinity));
+
+    graph.allEdges.forEach((edge) => {
+        const i = graph.allVertices.indexOf(edge.start);
+        const j = graph.allVertices.indexOf(edge.end);
+        dist[i][j] = edge.weight;
+    });
 }
 
 module.exports = floydWarshall;
