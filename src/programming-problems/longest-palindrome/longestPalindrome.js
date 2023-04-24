8829f388f13f7dbd73d0ebbaeeeb7eab36977a40
Evgeni Rusev <evgeni.n.rusev@gmail.com>
2023-04-24 18:27:17 +0300

    longest palindrome solutiong

--- src/programming-problems/longest-palindrome/longestPalindrome.js
+++ src/programming-problems/longest-palindrome/longestPalindrome.js
@@ -1,7 +1,30 @@
 // https://leetcode.com/problems/longest-palindrome
 
 function longestPalindrome(str) {
-    return 0;
+    if (str.length <= 1) {
+        return str.length;
+    }
+
+    const charCounts = str.split('').reduce((map, char) => {
+        if (!map[char])
+            map[char] = 0;
+        
+        map[char]++;
+        return map;
+    }, {});
+    
+    let oddNumbersCount = 0;
+    return (2 * Object.values(charCounts).reduce((sum, currentValue) => {
+        if (currentValue % 2) {
+            oddNumbersCount++;
+        }
+
+        if (currentValue != 1 && currentValue ) {
+            sum += parseInt(currentValue / 2);
+        }
+        
+        return sum;
+    }, 0)) + (oddNumbersCount > 0 ? 1 : 0);
 }
 
 module.exports = longestPalindrome;
