baabe3153381b85f13c3981624c866866617db16
Evgeni Rusev <evgeni.n.rusev@gmail.com>
2023-04-28 06:50:11 +0300

    n queens - complete canPlaceQueen

--- src/backtracking/n-queen-problem/nQueenProblem.js
+++ src/backtracking/n-queen-problem/nQueenProblem.js
@@ -4,16 +4,17 @@ function nQueenProblem(n) {
     const boards = new Array(n).fill([]);
 
     for (let row = 0; row < n; row++) {
-        recurse(row + 1, n, boards[0]);
+        backtrack(row + 1, n, boards[0]);
     }
 
     return boards;
 }
 
-function recurse(row, n, board) {
+function backtrack(row, n, board) {
     for (let col = 0; col < n; col++) {
         if (canPlaceQueen(row, col, board, n)) {
-
+            board[row][col] = 'Q';
+            backtrack(row + 1, n, board);
         }
     }
 }
@@ -22,10 +23,18 @@ function canPlaceQueen(row, col, board, n) {
     const highestIndex = Math.max(n, n - row);
 
     for (let i = 1; i < highestIndex; i++) {
-        if (board[i + row][col + i] != '.') {
+        const topLeft = board[row - i][col - i];
+        const bottomRight = board[i + row][col + i];
+        const topRight = board[row - i][col + i];
+        const bottomLeft = board[row + 1][col - i];
 
+        if (topLeft == 'Q' || bottomRight == 'Q' 
+        || topRight == 'Q' || bottomLeft == 'Q') {
+            return false;
         }
     }
+
+    return true;
 }
 
 // 33:
