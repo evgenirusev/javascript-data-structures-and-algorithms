ecae64b57baa3d72d748143e2642235d03c2838d
Evgeni Rusev <evgeni.n.rusev@gmail.com>
2023-04-30 08:13:50 +0300

    Correct solution for N Queens - Make the second unit test pass finally =)

--- src/backtracking/n-queen-problem/n-queen-problem.js
+++ src/backtracking/n-queen-problem/n-queen-problem.js
@@ -1,39 +1,54 @@
 // https://leetcode.com/problems/n-queens/
 
+// Possible solutions for n = 4
+//   0 1 2 3
+// 0 . Q . .
+// 1 . . . Q
+// 2 Q . . .
+// 3 . . Q .
+
+//   0 1 2 3
+// 0 . . Q .
+// 1 Q . . .
+// 2 . . . Q
+// 3 . Q . .
+
 function nQueenProblem(n) {
-    
-    const boards = Array(n).fill().map(() => Array(n).fill('.'));
-
-    for (let row = 0; row < n; row++) {
-        placeQueenOnRow(row, n, boards);
-    }
-
-    return boards;
+    const result = [];
+    const board = Array(n).fill().map(() => Array(n).fill('.'));
+    placeQueen(board, 0, n, result, 0);
+    return result;
 }
 
-function placeQueenOnRow(row, n, boards) {
-    if (row >= n)
-        return;
-
+function placeQueen(board, row, n, results, queensCount) {
     for (let col = 0; col < n; col++) {
-        if (canPlaceQueen(row, col, boards, n)) {
-            boards[row][col] = 'Q';
-            placeQueenOnRow(row + 1, n, boards);
+        if (canPlaceQueen(board, row, col, n)) {
+            board[row][col] = 'Q';
+            queensCount++;
+
+            if (row == n - 1 && queensCount == n) {
+                results.push(board.map(row => row.join('')));
+            } else {
+                placeQueen(board, row + 1, n, results, queensCount);
+            }
+
+            board[row][col] = '.';
+            queensCount--;
         }
     }
 }
 
-function canPlaceQueen(row, col, boards, n) {
+function canPlaceQueen(board, row, col, n) {
     const highestIndex = Math.max(row, n - row);
 
-    for (let i = 1; i < highestIndex; i++) {
-        const topCell = boards[row - i] && boards[row - i][col];
-        const bottomCell = boards[row + i] && boards[row + i][col];
-    
-        const topLeft = boards[row - i] && boards[row - i][col - i];
-        const bottomRight = boards[i + row] && boards[i + row][col + i];
-        const topRight = boards[row - i] && boards[row - i][col + i];
-        const bottomLeft = boards[row + 1] && boards[row + 1][col - i];
+    for (let i = 1; i <= highestIndex; i++) {
+        const topCell = board[row - i] && board[row - i][col];
+        const bottomCell = board[row + i] && board[row + i][col];
+
+        const topLeft = board[row - i] && board[row - i][col - i];
+        const bottomRight = board[i + row] && board[i + row][col + i];
+        const topRight = board[row - i] && board[row - i][col + i];
+        const bottomLeft = board[row + 1] && board[row + 1][col - i];
     
         if (topCell == 'Q' || bottomCell == 'Q' || topLeft == 'Q' 
             || bottomRight == 'Q' || topRight == 'Q' || bottomLeft == 'Q')
