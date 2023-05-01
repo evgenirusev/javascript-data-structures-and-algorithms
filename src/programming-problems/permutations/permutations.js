8c970017e7ace117ce3b7b4f49d4aa1351bb003f
Evgeni Rusev <evgeni.n.rusev@gmail.com>
2023-05-01 08:47:48 +0300

    Implement permutations solution via backtracking solution

--- src/programming-problems/permutations/permutations.js
+++ src/programming-problems/permutations/permutations.js
@@ -1,7 +1,25 @@
-https://leetcode.com/problems/permutations/
+// https://leetcode.com/problems/permutations/
 
-function permutations(array) {
-    
+function permutations(nums) {
+    const result = [];
+    backtrack([], nums, result);
+    return result;
 }
+
+function backtrack(curr, nums, result) {
+    if (curr.length === nums.length) {
+        result.push([...curr]);
+        return;
+    }
+  
+    for (let i = 0; i < nums.length; i++) {
+        if (curr.includes(nums[i]))
+            continue;
+
+        curr.push(nums[i]);
+        backtrack(curr, nums, result);
+        curr.pop();
+    }
+}  
 
 module.exports = permutations;
--- src/programming-problems/permutations/permutations.test.js
+++ src/programming-problems/permutations/permutations.test.js
@@ -2,30 +2,24 @@ const permutations = require('./permutations')
 
 describe("permutations", () => {
     it("returns all possible permutations for an array with distinct integers", () => {
-        const nums = [1, 2, 3];
-        const expectedOutput = [
+        expect(permutations([1, 2, 3])).toEqual([
             [1, 2, 3],
             [1, 3, 2],
             [2, 1, 3],
             [2, 3, 1],
             [3, 1, 2],
             [3, 2, 1],
-        ];
-        expect(permutations(nums)).toEqual(expectedOutput);
+        ]);
     });
 
     it("returns all possible permutations for an array with two distinct integers", () => {
-        const nums = [0, 1];
-        const expectedOutput = [
+        expect(permutations([0, 1])).toEqual([
             [0, 1],
             [1, 0],
-        ];
-        expect(permutations(nums)).toEqual(expectedOutput);
+        ]);
     });
 
     it("returns the same array as the only possible permutation for an array with one integer", () => {
-        const nums = [1];
-        const expectedOutput = [[1]];
-        expect(permutations(nums)).toEqual(expectedOutput);
+        expect(permutations([1])).toEqual([[1]]);
     });
 });
