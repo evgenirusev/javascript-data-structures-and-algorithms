e8054b1ecbfe2d5cc1570a0d210b4855393f444c
Evgeni Rusev <evgeni.n.rusev@gmail.com>
2023-05-06 18:14:59 +0300

    Delete excess folders

--- src/practice/practice.js
+++
@@ -1,5 +0,0 @@
-function execute() {
-
-}
-
-module.exports = execute;
--- src/practice/practice.test.js
+++
@@ -1,7 +0,0 @@
-const execute = require("./practice");
-
-describe("execute", () => {
-    it("should work as expected", () => {
-        execute();
-    });
-});
