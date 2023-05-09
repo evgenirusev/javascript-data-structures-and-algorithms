91c2841d0db188860c7bb5c8692a11b307a0155c
Evgeni Rusev <evgeni.n.rusev@gmail.com>
2023-05-09 11:15:22 +0300

    Add find disappeared nums hashing solution

--- src/arrays-and-hashing/find-disappeared-nums/find-disappeared-nums.js
+++ src/arrays-and-hashing/find-disappeared-nums/find-disappeared-nums.js
@@ -1,6 +1,14 @@
 function findDisappearedNumbers(nums) {
     const disappearedNums = [];
-    
+    const highestNum = nums.length;
+    const set = new Set(nums);
+
+    for (let i = 1; i <= highestNum; i++) {
+        if (!set.has(i)) {
+            disappearedNums.push(i);
+        }
+    }
+
     return disappearedNums;
 }
 
--- src/arrays-and-hashing/find-disappeared-nums/find-disappeared-nums.test.js
+++ src/arrays-and-hashing/find-disappeared-nums/find-disappeared-nums.test.js
@@ -1,17 +1,17 @@
-const findDisappearedNumbers = require('./findDisappearedNumbers');
+const findDisappearedNumbers = require('./find-disappeared-nums');
 
 describe('findDisappearedNumbers', () => {
-  test('Example 1: [4,3,2,7,8,2,3,1]', () => {
-    const nums = [4, 3, 2, 7, 8, 2, 3, 1];
-    const result = findDisappearedNumbers(nums);
-    expect(result).toEqual(expect.arrayContaining([5, 6]));
-    expect(result.length).toBe(2);
-  });
+    it('should find 5 and 6', () => {
+        const nums = [4, 3, 2, 7, 8, 2, 3, 1];
+        const result = findDisappearedNumbers(nums);
+        expect(result).toEqual(expect.arrayContaining([5, 6]));
+        expect(result.length).toBe(2);
+    });
 
-  test('Example 2: [1,1]', () => {
-    const nums = [1, 1];
-    const result = findDisappearedNumbers(nums);
-    expect(result).toEqual(expect.arrayContaining([2]));
-    expect(result.length).toBe(1);
-  });
+    it('should find 2', () => {
+        const nums = [1, 1];
+        const result = findDisappearedNumbers(nums);
+        expect(result).toEqual(expect.arrayContaining([2]));
+        expect(result.length).toBe(1);
+    });
 });
