a4cd5efb1fb7741fa217de02127c73b3b76169ab
Evgeni Rusev <evgeni.n.rusev@gmail.com>
2023-05-10 14:08:10 +0300

    Implement a better solution for find disappeared nums with constant space

--- src/arrays-and-hashing/find-disappeared-nums/find-disappeared-nums.js
+++ src/arrays-and-hashing/find-disappeared-nums/find-disappeared-nums.js
@@ -1,15 +1,33 @@
+// In place state management solution - O(n) time and O(1) space
 function findDisappearedNumbers(nums) {
-    const disappearedNums = [];
-    const highestNum = nums.length;
-    const set = new Set(nums);
+    for (let i = 0; i < nums.length; i++) {
+        const num = Math.abs(nums[i]);
+        nums[num - 1] = -Math.abs(nums[num - 1]);
+    }
 
-    for (let i = 1; i <= highestNum; i++) {
-        if (!set.has(i)) {
-            disappearedNums.push(i);
+    const disappearedNums = [];
+    for (let i = 0; i < nums.length; i++) {
+        if (Math.sign(nums[i]) > 0) {
+            disappearedNums.push(i + 1);
         }
     }
 
     return disappearedNums;
 }
+
+// Hashing solution - O(n) time and O(n) space
+// function findDisappearedNumbers(nums) {
+//     const disappearedNums = [];
+//     const highestNum = nums.length;
+//     const set = new Set(nums);
+
+//     for (let i = 1; i <= highestNum; i++) {
+//         if (!set.has(i)) {
+//             disappearedNums.push(i);
+//         }
+//     }
+
+//     return disappearedNums;
+// }
 
 module.exports = findDisappearedNumbers;
