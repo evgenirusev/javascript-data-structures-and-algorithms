6c0587385eb66b128402118bbe406f8d248fbd6d
Evgeni Rusev <evgeni.n.rusev@gmail.com>
2023-05-18 21:02:28 +0300

    implement kadane's algorithm

---
+++ src/dynamic-programming/1d/kadanes-algorithm/kadanes-algorithm.js
@@ -0,0 +1,13 @@
+function kadanes(arr) {
+    let maxSum = arr[0];
+    let currentSum = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+    currentSum = Math.max(arr[i], currentSum + arr[i]);
+    maxSum = Math.max(maxSum, currentSum);
+    }
+
+    return maxSum;
+}
+
+module.exports = kadanes;
--- src/dynamic-programming/1d/kadanes-algorithm/kadanes-algorithm.test.js
+++ src/dynamic-programming/1d/kadanes-algorithm/kadanes-algorithm.test.js
@@ -1,20 +1,16 @@
-const kadane = require('./kadanes-algorithm');
+const kadanes = require('./kadanes-algorithm');
 
-describe('Kadane algorithm', () => {
+describe('kadanes algorithm', () => {
   it('returns the maximum sum of a subarray', () => {
-    expect(kadane([-2, 1, -3, 4, -1, 2, 1, -5, 4])).toBe(6);
-    expect(kadane([1, 2, 3, 4, 5])).toBe(15);
-    expect(kadane([-1, -2, -3, -4, -5])).toBe(-1);
-    expect(kadane([0, 0, 0, 0, 0])).toBe(0);
-    expect(kadane([10, -2, 3, -4, 7, 2, -5])).toBe(16);
+    expect(kadanes([-2, 1, -3, 4, -1, 2, 1, -5, 4])).toBe(6);
+    expect(kadanes([1, 2, 3, 4, 5])).toBe(15);
+    expect(kadanes([-1, -2, -3, -4, -5])).toBe(-1);
+    expect(kadanes([0, 0, 0, 0, 0])).toBe(0);
+    expect(kadanes([10, -2, 3, -4, 7, 2, -5])).toBe(16);
   });
 
   it('returns the maximum sum when the array contains a single element', () => {
-    expect(kadane([-5])).toBe(-5);
-    expect(kadane([10])).toBe(10);
-  });
-
-  it('returns 0 when the array is empty', () => {
-    expect(kadane([])).toBe(0);
+    expect(kadanes([-5])).toBe(-5);
+    expect(kadanes([10])).toBe(10);
   });
 });
