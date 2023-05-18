7126e6328d2b86cd87f81ece253a8aea8d1f531d
Evgeni Rusev <evgeni.n.rusev@gmail.com>
2023-05-18 21:03:35 +0300

    format

--- src/dynamic-programming/1d/kadanes-algorithm/kadanes-algorithm.js
+++ src/dynamic-programming/1d/kadanes-algorithm/kadanes-algorithm.js
@@ -3,8 +3,8 @@ function kadanes(arr) {
     let currentSum = arr[0];
 
     for (let i = 1; i < arr.length; i++) {
-    currentSum = Math.max(arr[i], currentSum + arr[i]);
-    maxSum = Math.max(maxSum, currentSum);
+        currentSum = Math.max(arr[i], currentSum + arr[i]);
+        maxSum = Math.max(maxSum, currentSum);
     }
 
     return maxSum;
