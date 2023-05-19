1501fd0f6dee4a926f5e609f9575b53b9ea296bd
Evgeni Rusev <evgeni.n.rusev@gmail.com>
2023-05-19 08:19:26 +0300

    implement contains nearby duplicates

--- src/dynamic-programming/1d/sliding-window/contains-nearby-duplicates/contains-nearby-duplicates.js
+++ src/dynamic-programming/1d/sliding-window/contains-nearby-duplicates/contains-nearby-duplicates.js
@@ -1,7 +1,25 @@
 // https://leetcode.com/problems/contains-duplicate-ii/description/
 
 function containsNearbyDuplicate(arr, k) {
+    if (k === 0)
+        return false;
     
+    const set = new Set();
+    let L = 0;
+    set.add(arr[L]);
+    for (let i = 1; i < arr.length; i++) {
+        if (set.has(arr[i]))
+            return true;
+    
+        set.add(arr[i]);
+    
+        if (set.size > k) {
+            set.delete(arr[L]);
+            L++;
+        }
+    }
+
+    return false;
 }
 
 module.exports = containsNearbyDuplicate;
