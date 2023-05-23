44bb9212a7ce65b42ca74ac2146581c2ee81f31f
Evgeni Rusev <evgeni.n.rusev@gmail.com>
2023-05-23 10:59:32 +0300

    Add encode and decode strings problem

---
+++ src/arrays-and-hashing/encode-and-decode-strings/encodeAndDecodeStrings.test.js
@@ -0,0 +1,24 @@
+const { encode, decode } = require('./solution');
+
+describe('Leetcode Problem 271: Encode and Decode Strings', () => {
+    it('should encode and decode empty string array', () => {
+        const input = [];
+        const encoded = encode(input);
+        const decoded = decode(encoded);
+        expect(decoded).toEqual(input);
+    });
+
+    it('should encode and decode single string', () => {
+        const input = ['hello'];
+        const encoded = encode(input);
+        const decoded = decode(encoded);
+        expect(decoded).toEqual(input);
+    });
+
+    it('should encode and decode multiple strings', () => {
+        const input = ['hello', 'world', 'leetcode'];
+        const encoded = encode(input);
+        const decoded = decode(encoded);
+        expect(decoded).toEqual(input);
+    });
+});
