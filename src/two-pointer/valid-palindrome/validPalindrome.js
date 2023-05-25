26e7323fc9d7d9074032a0b5d6c5f0933f3c8c39
Evgeni Rusev <evgeni.n.rusev@gmail.com>
2023-05-25 19:11:41 +0300

    Implement 2 pointers solution

---
+++ src/two-pointer/valid-palindrome/validPalindrome.js
@@ -0,0 +1,26 @@
+var isPalindrome = function(s) {
+    for (let i = 0, j = s.length - 1; i < j; i++, j--) {
+        while (i < j && !isValidChar(s.charAt(i))) {
+            i++;
+        }
+
+        while (i < j && !isValidChar(s.charAt(j))) {
+            j--;
+        }
+
+        if (s.toLowerCase().charAt(i) != s.toLowerCase().charAt(j))
+            return false;
+    }
+
+    return true;
+};
+
+function isValidChar(c) {
+    const charCode = c.toLowerCase().charCodeAt();
+    return (charCode >= 'a'.charCodeAt()
+        && charCode <= 'z'.charCodeAt()) 
+        || (charCode >= '0'.charCodeAt()
+           && charCode <= '9'.charCodeAt());
+}
+
+module.exports = isPalindrome;
--- src/two-pointer/valid-palindrome/validPalindrome.test.js
+++ src/two-pointer/valid-palindrome/validPalindrome.test.js
@@ -1,31 +1,31 @@
-const validPalindrome = require('./validPalindrome');
+const isPalindrome = require('./validPalindrome');
 
 describe('Leetcode Problem 125: Valid Palindrome', () => {
     it('should return true for a valid palindrome', () => {
         const input = 'A man, a plan, a canal: Panama';
         const expected = true;
-        const result = func.isPalindrome(input);
+        const result = isPalindrome(input);
         expect(result).toBe(expected);
     });
 
     it('should return true for an empty string', () => {
         const input = '';
         const expected = true;
-        const result = func.isPalindrome(input);
+        const result = isPalindrome(input);
         expect(result).toBe(expected);
     });
 
     it('should return true for a single character', () => {
         const input = 'a';
         const expected = true;
-        const result = func.isPalindrome(input);
+        const result = isPalindrome(input);
         expect(result).toBe(expected);
     });
 
     it('should return false for a non-palindrome string', () => {
         const input = 'racecarx';
         const expected = false;
-        const result = func.isPalindrome(input);
+        const result = isPalindrome(input);
         expect(result).toBe(expected);
     });
 });
