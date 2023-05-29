eef8019c208114f601d92c1ba679986cb0ba6292
Evgeni Rusev <evgeni.n.rusev@gmail.com>
2023-05-29 23:24:55 +0300

    add analysis and update variable names and tests

--- src/two-pointer/three-sum/threeSum.js
+++ src/two-pointer/three-sum/threeSum.js
@@ -1,26 +1,32 @@
 /**
+ * https://leetcode.com/problems/3sum
+ * Solution using sorting
+ * Time complexity - O(n^2)
+ * Space complexity - O(n)
  * @param {number[]} nums
  * @return {number[][]}
  */
+
+// Cases:
+// [0,0,0]
+// [-1,0,0,2]
+// [-4,-1,-1,0]
+// [-4,-1,-1,0,1,2,4]
+// [-4,-1,-1,0,0,0,1,2,7,9,10,20]
+
+// Breaking down the problem:
+// When to update pointers:
+    // If sum(L, M, R) >= 0 -> decrement R
+    // If sum(L, M, R) < 0 -> increment M
+// Base cases for M:
+    // M < R
+// Base cases for L:
+    // L > 0
+
 var threeSum = function(nums) {
-    // [0,0,0]
-    // [-1,0,0,2]
-    // [-4,-1,-1,0]
-    // [-4,-1,-1,0,1,2,4]
-    // [-4,-1,-1,0,0,0,1,2,7,9,10,20]
-
-    // When to update pointers:
-        // If sum(L, M, R) >= 0 -> decrement R
-        // If sum(L, M, R) < 0 -> increment M
-    // Base cases for M:
-        // M < R
-    // Base cases for L:
-        // L > 0
-
     nums.sort((a, b) => a - b);
-    const usedCombinations = new Set();
+    const usedVariations = new Set();
 
-    //  [-1, -1, -4, 0, 1, 2]
     const result = [];
     let L = 0;
     let M = 1;
@@ -29,13 +35,13 @@ var threeSum = function(nums) {
         while (M < R) {
             const sum = nums[L] + nums[M] + nums[R];
             if (sum === 0) {
-                const combo = `${nums[L]}${nums[M]}${nums[R]}`;
-                if (!usedCombinations.has(combo)) {
+                const variation = `${nums[L]}${nums[M]}${nums[R]}`;
+                if (!usedVariations.has(variation)) {
                     result.push([nums[L], nums[M], nums[R]]);
-                    usedCombinations.add(combo);
+                    usedVariations.add(variation);
                 }
 
-                M++; // R-- it's the same
+                M++;
             } else if (sum > 0) {
                 R--;
             } else if (sum < 0) {
--- src/two-pointer/three-sum/threeSum.test.js
+++ src/two-pointer/three-sum/threeSum.test.js
@@ -2,32 +2,22 @@ const threeSum = require('./threeSum');
 
 describe('Three Sum', () => {
   it('should return the correct triplets', () => {
-      // Test Case 1
-      let nums = [-1, 0, 1, 2, -1, -4];
-      let expected = [
-          [-1, -1, 2],
-          [-1, 0, 1]
-      ];
-      expect(threeSum(nums)).toEqual(expect.arrayContaining(expected));
+      expect(threeSum([-1, 0, 1, 2, -1, -4])).toEqual(expect.arrayContaining([[-1, -1, 2], [-1, 0, 1]]));
+  });
 
-      // Test Case 2
-      nums = [];
-      expected = [];
-      expect(threeSum(nums)).toEqual(expect.arrayContaining(expected));
+  it('should return an empty array when input array is empty', () => {
+      expect(threeSum([])).toEqual(expect.arrayContaining([]));
+  });
 
-      // Test Case 3
-      nums = [0];
-      expected = [];
-      expect(threeSum(nums)).toEqual(expect.arrayContaining(expected));
+  it('should return an empty array when input array has only one element', () => {
+      expect(threeSum([0])).toEqual(expect.arrayContaining([]));
+  });
 
-      // Test Case 4
-      nums = [0, 0, 0, 0];
-      expected = [[0, 0, 0]];
-      expect(threeSum(nums)).toEqual(expect.arrayContaining(expected));
+  it('should return the correct triplet when input array has all elements as zeros', () => {
+      expect(threeSum([0, 0, 0, 0])).toEqual(expect.arrayContaining([[0, 0, 0]]));
+  });
 
-      // Test Case 5
-      nums = [1, 2, -2, -1];
-      expected = [];
-      expect(threeSum(nums)).toEqual(expect.arrayContaining(expected));
+  it('should return an empty array when no triplet sums to zero', () => {
+      expect(threeSum([1, 2, -2, -1])).toEqual(expect.arrayContaining([]));
   });
 });
