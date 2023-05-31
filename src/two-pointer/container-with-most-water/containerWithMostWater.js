53f862c943e9e466f0dc20c63cae7c5faa129f49
Evgeni Rusev <evgeni.n.rusev@gmail.com>
2023-05-31 08:57:35 +0300

    add implementation and analysis for container with most water

--- src/two-pointer/container-with-most-water/containerWithMostWater.js
+++ src/two-pointer/container-with-most-water/containerWithMostWater.js
@@ -1,3 +1,26 @@
+/**
+ * https://leetcode.com/problems/container-with-most-water
+ * Two pointer approach
+ * Time complexity - O(n)
+ * Space complexity - O(1)
+ * @param {number[]} height
+ * @return {number}
+ */
+function maxArea(height) {
+    let maxVolume = 0;
+    let l = 0, r = height.length - 1;
+    while (l < r) {
+        const volume = Math.min(height[r], height[l]) * (r - l);
+        maxVolume = Math.max(volume, maxVolume);
 
+        if (height[l] > height[r]) {
+            r--;
+        } else {
+            l++;
+        }
+    }
+
+    return maxVolume;
+};
 
 module.exports = maxArea;
