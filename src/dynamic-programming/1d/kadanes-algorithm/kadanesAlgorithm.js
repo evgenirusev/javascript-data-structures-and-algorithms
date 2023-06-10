e5a348e45730a21d632456202dfde5dbe3e17d49
Evgeni Rusev <evgeni.n.rusev@gmail.com>
2023-06-10 17:19:26 +0300

    move sliding-window to top level

--- src/dynamic-programming/1d/kadanes-algorithm/kadanesAlgorithm.js
+++ src/dynamic-programming/1d/kadanes-algorithm/kadanesAlgorithm.js
@@ -1,13 +1,13 @@
 function kadanes(arr) {
-    let maxSum = arr[0];
+    let globalMax = arr[0];
     let currentSum = arr[0];
 
     for (let i = 1; i < arr.length; i++) {
         currentSum = Math.max(arr[i], currentSum + arr[i]);
-        maxSum = Math.max(maxSum, currentSum);
+        globalMax = Math.max(globalMax, currentSum);
     }
 
-    return maxSum;
+    return globalMax;
 }
 
 module.exports = kadanes;
