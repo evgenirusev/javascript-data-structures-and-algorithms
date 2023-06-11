47557172efc615665ed0c9b87f5d4b210bdfebf7
Evgeni Rusev <evgeni.n.rusev@gmail.com>
2023-06-11 21:30:17 +0300

    format and implementation

--- src/data-structures/linked-list/problems/reverse-linked-list/reverseLinkedList.js
+++ src/data-structures/linked-list/problems/reverse-linked-list/reverseLinkedList.js
@@ -1,25 +1,26 @@
 /**
+ * https://leetcode.com/problems/reverse-linked-list/editorial/
+ * 
  * Definition for singly-linked list.
  * function ListNode(val, next) {
  *     this.val = (val===undefined ? 0 : val)
  *     this.next = (next===undefined ? null : next)
  * }
- */
-/**
+ *
  * @param {ListNode} head
  * @return {ListNode}
  */
-const reverseList = function (head) {
+function reverseList(head) {
     let prev = null;
     let curr = head;
     while (curr != null) {
-      const temp = curr.next;
-      curr.next = prev;
-      prev = curr;
-      curr = temp;
+        const temp = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = temp;
     }
   
     return prev;
-  };
+};
   
-  module.exports = reverseList;
+module.exports = reverseList;
