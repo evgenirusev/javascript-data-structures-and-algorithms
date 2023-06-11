16893651748cd33a972ea0514e1cecf5731a04b2
Evgeni Rusev <evgeni.n.rusev@gmail.com>
2023-06-11 21:29:24 +0300

    add reverse linked list implementation

---
+++ src/data-structures/linked-list/problems/reverse-linked-list/reverseLinkedList.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const reverseList = function (head) {
+    let prev = null;
+    let curr = head;
+    while (curr != null) {
+      const temp = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = temp;
+    }
+  
+    return prev;
+  };
+  
+  module.exports = reverseList;
