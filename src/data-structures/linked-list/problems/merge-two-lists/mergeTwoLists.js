7a8648a12ba5bb76b5cbf66a91c7360662ba9126
Evgeni Rusev <evgeni.n.rusev@gmail.com>
2023-06-12 14:00:40 +0300

    Add recursive solution

--- src/data-structures/linked-list/problems/merge-two-lists/mergeTwoLists.js
+++ src/data-structures/linked-list/problems/merge-two-lists/mergeTwoLists.js
@@ -1,5 +1,6 @@
 /**
  * https://leetcode.com/problems/merge-two-sorted-lists
+ * Iterative solution
  * Time complexity - O(N + M)
  * Space complexity - O(1)
  * @param {ListNode} list1
@@ -24,6 +25,21 @@ function mergeTwoLists(l1, l2) {
     prev.next = l1 === null ? l2 : l1;
 
     return tempHead.next;
+}
+
+// Recursive solution
+function mergeTwoLists(l1, l2) {
+    if (l1 === null) {
+        return l2;
+    } else if (l2 === null) {
+        return l1;
+    } else if (l1.val < l2.val) {
+        l1.next = mergeTwoLists(l1.next, l2);
+        return l1;
+    } else {
+        l2.next = mergeTwoLists(l1, l2.next);
+        return l2;
+    }
 }
 
 // Definition for singly-linked list.
