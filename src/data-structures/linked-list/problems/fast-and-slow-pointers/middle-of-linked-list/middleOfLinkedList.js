625b2d93fe68a7962860cb6f578c9fe92dfd0386
Evgeni Rusev <evgeni.n.rusev@gmail.com>
2023-06-17 19:27:27 +0300

    add fast and slow pointer solution for middle Of Linked list

---
+++ src/data-structures/linked-list/problems/fast-and-slow-pointers/middle-of-linked-list/middleOfLinkedList.js
@@ -0,0 +1,26 @@
+/**
+ * https://leetcode.com/problems/middle-of-the-linked-list
+ * Fast and slow pointer solution
+ * Time complexity - O(n)
+ * Space complexity - O(1)
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+function middleNode(head) {
+    let slow = head;
+    let fast = head.next;
+
+    while (fast != null) {
+        slow = slow.next;
+        fast = fast.next ? fast.next.next : null;
+    }
+
+    return slow;
+};
+
+function ListNode(val, next) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+}
+
+module.exports = { ListNode, middleNode };
