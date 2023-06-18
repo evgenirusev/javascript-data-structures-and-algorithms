6f047f35f335f3b1fa9926af00877c16a930b269
Evgeni Rusev <evgeni.n.rusev@gmail.com>
2023-06-18 13:52:55 +0300

    Add tests for addTwoNumbers

---
+++ src/data-structures/linked-list/problems/add-two-numbers/addTwoNumbers.test.js
@@ -0,0 +1,53 @@
+const { ListNode, addTwoNumbers } = require('./addTwoNumbers');
+
+describe("addTwoNumbers", () => {
+    it("should return the sum of two single-digit numbers", () => {
+      const l1 = new ListNode(2);
+      const l2 = new ListNode(3);
+      const expected = new ListNode(5);
+      expect(addTwoNumbers(l1, l2)).toEqual(expected);
+    });
+  
+    it("should return the sum of two multi-digit numbers", () => {
+      const l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+      const l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+      const expected = new ListNode(7, new ListNode(0, new ListNode(8)));
+      expect(addTwoNumbers(l1, l2)).toEqual(expected);
+    });
+  
+    it("should handle the sum of two lists with leading zeros", () => {
+      const l1 = new ListNode(0);
+      const l2 = new ListNode(0);
+      const expected = new ListNode(0);
+      expect(addTwoNumbers(l1, l2)).toEqual(expected);
+    });
+  
+    it("should handle the sum of two large numbers", () => {
+      const l1 = new ListNode(
+        9,
+        new ListNode(9),
+        new ListNode(9),
+        new ListNode(9),
+        new ListNode(9),
+        new ListNode(9),
+        new ListNode(9)
+      );
+      const l2 = new ListNode(
+        9,
+        new ListNode(9),
+        new ListNode(9),
+        new ListNode(9)
+      );
+      const expected = new ListNode(
+        8,
+        new ListNode(9),
+        new ListNode(9),
+        new ListNode(9),
+        new ListNode(0),
+        new ListNode(0),
+        new ListNode(0),
+        new ListNode(1)
+      );
+      expect(addTwoNumbers(l1, l2)).toEqual(expected);
+    });
+});
