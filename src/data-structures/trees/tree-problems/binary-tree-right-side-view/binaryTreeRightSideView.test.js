5124fd0bcb6860e07f55f77a511577091ff789d9
Evgeni Rusev <evgeni.n.rusev@gmail.com>
2023-06-25 21:08:13 +0300

    Add binary tree right sde view solution

---
+++ src/data-structures/trees/tree-problems/binary-tree-right-side-view/binaryTreeRightSideView.test.js
@@ -0,0 +1,45 @@
+const rightSideView = require('./rightSideView');
+const TreeNode = require('./TreeNode');
+
+describe('rightSideView', () => {
+  it('should return an empty array for an empty tree', () => {
+    const root = null;
+    const expected = [];
+    const result = rightSideView(root);
+    expect(result).toEqual(expected);
+  });
+
+  it('should return the correct right side view for a tree with a single node', () => {
+    const root = new TreeNode(1);
+    const expected = [1];
+    const result = rightSideView(root);
+    expect(result).toEqual(expected);
+  });
+
+  it('should return the correct right side view for a balanced tree', () => {
+    // Create a balanced tree
+    const root = new TreeNode(1);
+    root.left = new TreeNode(2);
+    root.right = new TreeNode(3);
+    root.left.left = new TreeNode(4);
+    root.left.right = new TreeNode(5);
+    root.right.left = new TreeNode(6);
+    root.right.right = new TreeNode(7);
+
+    const expected = [1, 3, 7];
+    const result = rightSideView(root);
+    expect(result).toEqual(expected);
+  });
+
+  it('should return the correct right side view for an unbalanced tree', () => {
+    // Create an unbalanced tree
+    const root = new TreeNode(1);
+    root.left = new TreeNode(2);
+    root.left.left = new TreeNode(4);
+    root.left.left.right = new TreeNode(5);
+
+    const expected = [1, 2, 4, 5];
+    const result = rightSideView(root);
+    expect(result).toEqual(expected);
+  });
+});
