6c170510434a0e79db4e4f72182b0063d1a5e81f
Evgeni Rusev <evgeni.n.rusev@gmail.com>
2023-06-26 22:07:17 +0300

    unit tests for count good nodes in binary tree

---
+++ src/data-structures/trees/tree-problems/count-good-nodes-in-binary-tree/countGoodNodesInBinaryTree.test.js
@@ -0,0 +1,29 @@
+const countGoodNodesInBinaryTree = require('./countGoodNodesInBinaryTree');
+
+describe("countGoodNodesInBinaryTree", () => {
+    it('returns 0 for empty tree', () => {
+        expect(countGoodNodesInBinaryTree(null)).toBe(0);
+    });
+
+    it('returns 1 for single node', () => {
+        const root = { val: 5, left: null, right: null };
+        expect(countGoodNodesInBinaryTree(root)).toBe(1);
+    });
+
+    it('returns correct count for a binary tree', () => {
+        const root = {
+            val: 3,
+            left: {
+                val: 1,
+                left: { val: 3, left: null, right: null },
+                right: null
+            },
+            right: {
+                val: 4,
+                left: { val: 1, left: null, right: null },
+                right: null
+            }
+        };
+        expect(countGoodNodesInBinaryTree(root)).toBe(4);
+    });
+});
