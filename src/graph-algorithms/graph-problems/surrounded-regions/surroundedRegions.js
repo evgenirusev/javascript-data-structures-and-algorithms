91c35edfbc3dcb0ae5c03208399d3ba85cc6e47a
evgenirusev <evgeni.n.rusev@gmail.com>
2023-07-08 16:31:16 +0300

    add dfs solution

---
+++ src/graph-algorithms/graph-problems/surrounded-regions/surroundedRegions.js
@@ -0,0 +1,50 @@
+/**
+ * @param {character[][]} board
+ * @return {void} Do not return anything, modify board in-place instead.
+ */
+var surroundedRegions = function(board) {
+    const rows = board.length;
+    const cols = board[0].length;
+
+    const visited = new Set();
+    for (let row = 1; row < rows - 1; row++) {
+        for (let col = 1; col < cols - 1; col++) {
+            if (board[row][col] === "O") {
+                const currentVisited = new Set();
+                if (canBeCaptured(currentVisited, board, row, col, rows, cols)) {
+                    currentVisited.forEach(cell => {
+                        const [r, c] = cell.split(',');
+                        board[parseInt(r)][parseInt(c)] = "X";
+                    });
+                } else {
+                    for (const cell of currentVisited) {
+                        visited.add(cell);
+                    }
+                }
+            }
+        }
+    }
+};
+
+function canBeCaptured(cellsForCapture, board, row, col, rows, cols) {
+    if (row <= 0 || col <= 0 || row >= rows - 1 || col >= cols - 1)
+        return false;
+
+    cellsForCapture.add(`${row},${col}`);
+
+    for (const [r, c] of getNeighbors(row, col)) {
+        if (!cellsForCapture.has(`${r},${c}`) && board[r][c] == "O") {
+            if (!canBeCaptured(cellsForCapture, board, r, c, rows, cols)) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+function getNeighbors(row, col) {
+    return [[0, 1], [0, -1], [1, 0], [-1, 0]].map(cell => [row + cell[0], col + cell[1]]);
+}
+
+module.exports = surroundedRegions;
