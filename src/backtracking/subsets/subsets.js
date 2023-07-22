2fb1d43d9e0efddbf29cb971c9d26ffb2aa68ddd
evgenirusev <evgeni.n.rusev@gmail.com>
2023-07-22 11:59:52 +0300

    Add dfs approach for subset generation

--- src/backtracking/subsets/subsets.js
+++ src/backtracking/subsets/subsets.js
@@ -1,11 +1,37 @@
 /**
- * Cascading solution
+ * DFS approach
  * Time complexity - O(N * 2N)
  * Space complexity - O(N * 2N)
  * https://leetcode.com/problems/subsets
  * @param {number[]} nums
  * @return {number[][]}
  */
+function subsets(nums) {
+    const res = [];
+    const subset = [];
+
+    function dfs(i) {
+        if (i >= nums.length) {
+            res.push([...subset]);
+            return;
+        }
+        
+        subset.push(nums[i]);
+        dfs(i + 1);
+        
+        subset.pop();
+        dfs(i + 1);
+    }
+
+    dfs(0);
+    return res;
+}
+
+/*
+* Cascading solution
+* Time complexity - O(N * 2N)
+* Space complexity - O(N * 2N)
+*/
 function subsets(nums) {
     let result = [[]];
 
