b7b4ddebe47127b02718ec9b3ef9142c66b423f0
evgenirusev <evgeni.n.rusev@gmail.com>
2023-07-25 10:24:47 +0300

    Add climbing stairs solution

---
+++ src/dynamic-programming/1d/climbing-stairs/climbingStairs.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    if (n == 2)
+        return 2;
+    if (n == 1)
+        return 1;
+    
+    const memo = {};
+    memo[n] = 1;
+    memo[n - 1] = 1;
+
+    for (let i = n - 2; i >= 0; i--) {
+        memo[i] = memo[i + 1] + memo[i + 2];
+    }
+
+    return memo[0];
+};
