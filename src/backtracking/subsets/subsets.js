637fdf77f9829b493f09581bc05633a29523e215
evgenirusev <evgeni.n.rusev@gmail.com>
2023-10-05 12:40:37 +0300

    Subsets iterative approach

--- src/backtracking/subsets/subsets.js
+++ src/backtracking/subsets/subsets.js
@@ -1,5 +1,5 @@
 /**
- * DFS approach
+ * Iterative approach
  * Time complexity - O(N * 2N)
  * Space complexity - O(N * 2N)
  * https://leetcode.com/problems/subsets
@@ -7,25 +7,53 @@
  * @return {number[][]}
  */
 function subsets(nums) {
-    const res = [];
-    const subset = [];
+    let output = [];
+    let n = 0;
+    let k = 0;
 
-    function dfs(i) {
-        if (i >= nums.length) {
-            res.push([...subset]);
-            return;
+    function backtrack(first, curr, nums) {
+        if (curr.length === k) {
+          output.push([...curr]);
+          return;
         }
-        
-        subset.push(nums[i]);
-        dfs(i + 1);
-        
-        subset.pop();
-        dfs(i + 1);
+          
+        for (let i = first; i < n; ++i) {
+          curr.push(nums[i]);
+          backtrack(i + 1, curr, nums);
+          curr.pop();
+        }
+      }
+      
+    n = nums.length;
+    
+    for (k = 0; k < n + 1; ++k) {
+      backtrack(0, [], nums);
     }
 
-    dfs(0);
-    return res;
+    return output;
 }
+
+// Non-iterative approach:
+// function subsets(nums) {
+//     const res = [];
+//     const subset = [];
+
+//     function dfs(i) {
+//         if (i >= nums.length) {
+//             res.push([...subset]);
+//             return;
+//         }
+        
+//         subset.push(nums[i]);
+//         dfs(i + 1);
+        
+//         subset.pop();
+//         dfs(i + 1);
+//     }
+
+//     dfs(0);
+//     return res;
+// }
 
 /*
 * Cascading solution
