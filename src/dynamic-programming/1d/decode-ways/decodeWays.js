1545b84797ab869d9b664a8851a655dcf37f5921
evgenirusev <evgeni.n.rusev@gmail.com>
2023-10-16 18:38:32 +0300

    Add decode ways solution

--- src/dynamic-programming/1d/decode-ways/decodeWays.js
+++ src/dynamic-programming/1d/decode-ways/decodeWays.js
@@ -1,4 +1,21 @@
 function numDecodings(s) {
+    const dp = new Array(s.length + 1).fill(0);
+    dp[0] = 1;
+
+    dp[1] = s.charAt(0) === '0' ? 0 : 1;
+
+    for (let i = 2; i < dp.length; i++) {
+        if (s.charAt(i - 1) !== '0') {
+            dp[i] = dp[i - 1];
+        }
+
+        const twoDigit = parseInt(s.substring(i - 2, i));
+        if (twoDigit >= 10 && twoDigit <= 26) {
+            dp[i] += dp[i - 2];
+        }
+    }
+
+    return dp[s.length];
 }
 
 module.exports = numDecodings;
