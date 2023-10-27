e5f94024fe0da03a8308aac93cb09d1eedf87610
evgenirusev <evgeni.n.rusev@gmail.com>
2023-10-27 17:05:13 +0300

    Add longest common subsequence implementation using dp

---
+++ src/dynamic-programming/1d/longest-common-subsequence/longestCommonSubsequence.js
@@ -0,0 +1,12 @@
+function lengthOfLIS(nums) {
+    const LIS = Array(nums.length).fill(1);
+    
+    for (let i = nums.length - 1; i >= 0; i--)
+        for (let j = i + 1; j < nums.length; j++)
+            if (nums[i] < nums[j])
+                LIS[i] = Math.max(LIS[i], 1 + LIS[j]);
+
+    return Math.max(...LIS);
+}
+
+module.exports = lengthOfLIS;
