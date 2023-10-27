723cd90c2b088585c69233b5d01c9fe5b1892f51
evgenirusev <evgeni.n.rusev@gmail.com>
2023-10-27 17:06:29 +0300

    Add longest common subsequence implementation documentation

--- src/dynamic-programming/1d/longest-common-subsequence/longestCommonSubsequence.js
+++ src/dynamic-programming/1d/longest-common-subsequence/longestCommonSubsequence.js
@@ -1,6 +1,13 @@
+/**
+ * https://leetcode.com/problems/longest-increasing-subsequence
+ * @param {number[]} nums
+ * @return {number}
+ * Time complexity = O(n^2)
+ * Space complexity = O(n)
+ */
 function lengthOfLIS(nums) {
     const LIS = Array(nums.length).fill(1);
-    
+
     for (let i = nums.length - 1; i >= 0; i--)
         for (let j = i + 1; j < nums.length; j++)
             if (nums[i] < nums[j])
