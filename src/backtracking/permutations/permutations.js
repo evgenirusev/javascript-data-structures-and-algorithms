d03a8c209eb3d1e2798282e1914f00124b522779
evgenirusev <evgeni.n.rusev@gmail.com>
2023-11-05 12:32:09 +0200

    Add permutations alternative solution

--- src/backtracking/permutations/permutations.js
+++ src/backtracking/permutations/permutations.js
@@ -1,4 +1,6 @@
 // https://leetcode.com/problems/permutations/
+// Space complexity - O(N!)
+// Exclude index approach:
 function permutations(nums) {
     const result = [];
     backtrack([], nums, result);
@@ -20,5 +22,28 @@ function backtrack(curr, nums, result) {
         curr.pop();
     }
 }
+
+// Swap approach:
+// function backtrack(n, nums, output, first) {
+//     if (first === n) {
+//         output.push([...nums]);
+//     }
+      
+//     for (let i = first; i < n; i++) {
+//         [nums[first], nums[i]] = [nums[i], nums[first]];
+//         backtrack(n, nums, output, first + 1);
+//         [nums[first], nums[i]] = [nums[i], nums[first]];
+//     }
+// }
+  
+// function permute(nums) {
+//     const output = [];
+
+//     const numbersList = [...nums];
+  
+//     const n = nums.length;
+//     backtrack(n, numbersList, output, 0);
+//     return output;
+// }
 
 module.exports = permutations;
