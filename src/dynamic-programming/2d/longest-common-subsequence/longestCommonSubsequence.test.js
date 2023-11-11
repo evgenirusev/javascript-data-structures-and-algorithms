b79f670a66b694c7cb42bbaaea5163d21d5f3ea6
evgenirusev <evgeni.n.rusev@gmail.com>
2023-11-11 10:41:05 +0200

    Add lcs additional unit test

--- src/dynamic-programming/2d/longest-common-subsequence/longestCommonSubsequence.test.js
+++ src/dynamic-programming/2d/longest-common-subsequence/longestCommonSubsequence.test.js
@@ -1,7 +1,11 @@
 const longestCommonSubsequence = require('./longestCommonSubsequence');
 
 describe("longestCommonSubsequence", () => {
-    it('should return the length of the common subsequence', () => {
+    it('should return the lcs with the simplest case', () => {
         expect(longestCommonSubsequence("abcde", "ace")).toBe(3);
+    });
+
+    it('should return the lcs with case where the first string must be compared to second', () => {
+        expect(longestCommonSubsequence("abcdefghij", "ecdgi")).toBe(4);
     });
 });
