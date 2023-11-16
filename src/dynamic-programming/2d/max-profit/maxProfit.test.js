6ac5cf2cc75de100256b07b1e648967aadb034e7
evgenirusev <evgeni.n.rusev@gmail.com>
2023-11-16 18:22:16 +0200

    Add failing unit test for max profit

--- src/dynamic-programming/2d/max-profit/maxProfit.test.js
+++ src/dynamic-programming/2d/max-profit/maxProfit.test.js
@@ -5,4 +5,9 @@ describe("maxProfit", () => {
         const result = maxProfit([1,2,3,0,2]);
         expect(result).toEqual(3);
     });
+
+    it('should return the max profit', () => {
+        const result = maxProfit([1,2,4]);
+        expect(result).toEqual(3);
+    });
 });
