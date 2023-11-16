8187e9c8b5a1f740fda95af9c16d933ebdf36657
evgenirusev <evgeni.n.rusev@gmail.com>
2023-11-16 16:12:28 +0200

    add Top Down DP backtracking solution partially working

--- src/dynamic-programming/2d/max-profit/maxProfit.js
+++ src/dynamic-programming/2d/max-profit/maxProfit.js
@@ -2,6 +2,50 @@
  * @param {number[]} prices
  * @return {number}
  */
+
+// Initial approach with top down DP:
+var maxProfit = function(prices) {
+    // action: 0 - buy, 1 - sell, 2 - cooldown
+    const cache = Array.from({ length: prices.length },
+        () => Array.from({ length: 3 }).fill(null)
+    );
+
+    function dfs(i, buyIndex, action, value) {
+        if (i === prices.length)
+            return value;
+
+        if (cache[i][action] != null)
+            return cache[i][action];
+
+        let result = value;
+
+        // buy
+        if (action === 0) {
+            result = Math.max(
+                dfs(i + 1, i, 1, value),
+                dfs(i + 1, i, 2, value),
+            );
+        // sell
+        } else if (action == 1) {
+            result = (prices[i] - prices[buyIndex]) + dfs(i + 1, -1, 2, value);
+        // cooldown
+        } else {
+            result = Math.max(
+                dfs(i + 1, buyIndex, 0, value),
+                dfs(i + 1, buyIndex, 0, value)
+            )
+        }
+
+        cache[i][action] = result;
+        return result;
+    }
+
+    return Math.max(
+        dfs(0, -1, 0, 0),
+        dfs(0, -1, 2, 0)
+    );
+};
+
 // Initial brute force backtracking solution with dfs
 var maxProfit = function(prices) {
     // action: 0 - buy, 1 - sell, 2 - cooldown
