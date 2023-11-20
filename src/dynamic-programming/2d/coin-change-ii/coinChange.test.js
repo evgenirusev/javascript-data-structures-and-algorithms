fecd4759ad498a83ebef60c58a9732a68e0baaee
evgenirusev <evgeni.n.rusev@gmail.com>
2023-11-20 19:00:47 +0200

    Add timeout unit test

--- src/dynamic-programming/2d/coin-change-ii/coinChange.test.js
+++ src/dynamic-programming/2d/coin-change-ii/coinChange.test.js
@@ -8,4 +8,8 @@ describe("coinChange", () => {
   it('should return 0 for amount = 3, coins = [2]', () => {
     expect(change(3, [2])).toBe(0);
   });
+
+  it('should correctly calculate coin change without timeout', () => {
+    expect(change(500, [3,5,7,8,9,10,11])).toBe(150);
+  });
 });
