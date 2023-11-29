2456ddaa01c80c6c7a566b26e47380db56010ee4
evgenirusev <evgeni.n.rusev@gmail.com>
2023-11-29 19:19:51 +0200

    Add DFS level order traversal

--- src/data-structures/trees/tree-problems/level-order-traversal/levelOrderTraversal.js
+++ src/data-structures/trees/tree-problems/level-order-traversal/levelOrderTraversal.js
@@ -12,24 +12,48 @@ var levelOrder = function(root) {
         return [];
 
     const levelResults = [];
-    const queue = new Queue();
-    queue.enqueue(root);
-    while (!queue.isEmpty) {
-        const levelQueueLength = queue.length;
-        const levelResult = [];
-        for (let i = 0; i < levelQueueLength; i++) {
-            const node = queue.dequeue();
-            levelResult.push(node.val);
-            if (node.left)
-                queue.enqueue(node.left)
-            if (node.right)
-                queue.enqueue(node.right)
-        }
 
-        levelResults.push(levelResult);
+    function dfs(node, level) {
+        if (!node)
+            return;
+
+        if (!levelResults[level])
+            levelResults[level] = [node.val]
+        else
+            levelResults[level].push(node.val);
+        
+        dfs(node.left, level + 1);
+        dfs(node.right, level + 1);
     }
+
+    dfs(root, 0);
 
     return levelResults;
 };
+
+// var levelOrder = function(root) {
+//     if (!root)
+//         return [];
+
+//     const levelResults = [];
+//     const queue = new Queue();
+//     queue.enqueue(root);
+//     while (!queue.isEmpty) {
+//         const levelQueueLength = queue.length;
+//         const levelResult = [];
+//         for (let i = 0; i < levelQueueLength; i++) {
+//             const node = queue.dequeue();
+//             levelResult.push(node.val);
+//             if (node.left)
+//                 queue.enqueue(node.left)
+//             if (node.right)
+//                 queue.enqueue(node.right)
+//         }
+
+//         levelResults.push(levelResult);
+//     }
+
+//     return levelResults;
+// };
 
 module.exports = levelOrder;
