850345b90222ca15a8a259b124b524edc6dd02b8
evgenirusev <evgeni.n.rusev@gmail.com>
2023-12-04 19:38:18 +0200

    combinations implementation with backtrackings

--- src/backtracking/combinations/combinations.js
+++ src/backtracking/combinations/combinations.js
@@ -1,4 +1,24 @@
 function combine(n, k) {
+    function backtrack(curr, firstNum) {
+        if (curr.length === k) {
+            res.push([...curr]);
+            return;
+        }
+
+        const need = k - curr.length;
+        const remain = n - firstNum + 1;
+        const available = remain - need;
+        
+        for (let num = firstNum; num <= firstNum + available; num++) {
+            curr.push(num);
+            backtrack(curr, num + 1);
+            curr.pop();
+        }
+    }
+
+    const res = [];
+    backtrack([], 1);
+    return res;
 }
 
 module.exports = combine;
