9a03252d7abeb04557bd7de0c1e1325effc1e4d8
evgenirusev <evgeni.n.rusev@gmail.com>
2023-12-05 11:01:09 +0200

    move test to botom

--- src/graph-algorithms/shortest-path/dijkstra/dijkstra.test.js
+++ src/graph-algorithms/shortest-path/dijkstra/dijkstra.test.js
@@ -6,13 +6,6 @@ const fs = require("fs");
 const Edge = require('../../../data-structures/graphs/edge/Edge');
 
 describe('getShortestPaths', () => {
-    it('should return a list of shortest paths', () => {
-        const graph = graphMockGenerators.generateWeightedGraph();
-
-        expect(getShortestPaths(graph, graph.getVertex("k1")))
-            .toEqual({'k1': 0, 'k2': 6, 'k3': 3, 'k6': 10, 'k5': 5, 'k4': 7, 'k7': 11, 'k8': 9, 'k9': 9, 'k12': 8});
-    });
-
     // PROBLEM DEFINITION:
     // The file contains an adjacency list representation of an undirected weighted graph with 200 vertices labeled 1 to 200.  Each row consists of the node tuples that are adjacent to that particular vertex along with the length of that edge. For example, the 6th row has 6 as the first entry indicating that this row corresponds to the vertex labeled 6. The next entry of this row "141,8200" indicates that there is an edge between vertex 6 and vertex 141 that has length 8200.  The rest of the pairs of this row indicate the other vertices adjacent to vertex 6 and the lengths of the corresponding edges.
     // Your task is to run Dijkstra's shortest-path algorithm on this graph, using 1 (the first vertex) as the source vertex, and to compute the shortest-path distances between 1 and every other vertex of the graph. If there is no path between a vertex v and vertex 1, we'll define the shortest-path distance between 1 and v to be 1000000. 
@@ -58,5 +51,12 @@ describe('getShortestPaths', () => {
         expect(shortestPaths["165"]).toEqual(2442);
         expect(shortestPaths["188"]).toEqual(2505);
         expect(shortestPaths["197"]).toEqual(3068);
+    });
+
+    it('should return a list of shortest paths', () => {
+        const graph = graphMockGenerators.generateWeightedGraph();
+
+        expect(getShortestPaths(graph, graph.getVertex("k1")))
+            .toEqual({'k1': 0, 'k2': 6, 'k3': 3, 'k6': 10, 'k5': 5, 'k4': 7, 'k7': 11, 'k8': 9, 'k9': 9, 'k12': 8});
     });
 });
