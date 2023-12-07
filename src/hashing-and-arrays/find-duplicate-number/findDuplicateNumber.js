c289f1db30d5ead3be9491385bdc7f23139b0f7b
evgenirusev <evgeni.n.rusev@gmail.com>
2023-12-08 01:34:41 +0200

    Add findDuplicateNumber best solution

--- src/hashing-and-arrays/find-duplicate-number/findDuplicateNumber.js
+++ src/hashing-and-arrays/find-duplicate-number/findDuplicateNumber.js
@@ -1,4 +1,15 @@
 var findDuplicate = function(nums) {
+    const temp = nums[0];
+    for (let i = 0; i < nums.length; i++) {
+        const num = Math.abs(nums[i]);
+
+        if (nums[num - 1] < 0)
+            return num;
+        
+        nums[num - 1] = -nums[num - 1];
+    }
+
+    return temp;
 };
 
 module.exports = findDuplicate;
