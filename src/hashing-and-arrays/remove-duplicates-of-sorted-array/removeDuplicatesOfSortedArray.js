c7b693ff2d29924b3c020115ad0129cb1aeb5f96
evgenirusev <evgeni.n.rusev@gmail.com>
2023-12-08 01:52:33 +0200

    Add remove duplicates implementation

--- src/hashing-and-arrays/remove-duplicates-of-sorted-array/removeDuplicatesOfSortedArray.js
+++ src/hashing-and-arrays/remove-duplicates-of-sorted-array/removeDuplicatesOfSortedArray.js
@@ -1,4 +1,12 @@
 var removeDuplicates = function(nums) {
+    let insertIndex = 1;
+    for(let i = 1; i < nums.length; i++){
+        if(nums[i - 1] != nums[i]){  
+            nums[insertIndex] = nums[i];
+            insertIndex++;
+        }
+    }
+    return insertIndex;
 };
 
 module.exports = removeDuplicates;
