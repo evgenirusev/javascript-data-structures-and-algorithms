31e65b7d1dc966793801ef817d5521f2b80d85c2
evgenirusev <evgeni.n.rusev@gmail.com>
2023-12-09 21:51:17 +0200

    Add solution for decodeStrings

--- src/data-structures/stacks/stack-problems/decode-strings/decodeStrings.js
+++ src/data-structures/stacks/stack-problems/decode-strings/decodeStrings.js
@@ -3,6 +3,28 @@
  * @return {string}
  */
 function decodeStrings(s) {
+    const stack = [];
+
+    for (let char of s) {
+        if (char !== "]") {
+            stack.push(char);
+        } else {
+            let subStr = "";
+            while (stack[stack.length - 1] !== "[") {
+                subStr = stack.pop() + subStr;
+            }
+            stack.pop();
+
+            let multiplier = "";
+            while (stack.length > 0 && !isNaN(stack[stack.length - 1])) {
+                multiplier = stack.pop() + multiplier;
+            }
+
+            stack.push(subStr.repeat(Number(multiplier)));
+        }
+    }
+
+    return stack.join("");
 }
 
 module.exports = decodeStrings;
