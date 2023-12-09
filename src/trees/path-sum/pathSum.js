c7bd3f5d7dbd5f9f3740c9da4bef8743cd06a874
evgenirusev <evgeni.n.rusev@gmail.com>
2023-12-09 21:45:18 +0200

    Add solution for hasPathSum

---
+++ src/trees/path-sum/pathSum.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function(root, sum) {
+    if (root == null)
+      return false;
+
+    sum -= root.val;
+    if ((root.left == null) && (root.right == null))
+      return (sum == 0);
+      
+    return hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
+};
+
+module.exports = hasPathSum;
