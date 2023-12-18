bda09d4f1342c8f860c9b97a4158b693e1eaeb44
evgenirusev <evgeni.n.rusev@gmail.com>
2023-12-18 23:32:02 +0200

    Add binary tree maximum path sum documentation

--- src/data-structures/trees/tree-problems/binary-tree-maximum-path-sum/binaryTreeMaximumPathSum.js
+++ src/data-structures/trees/tree-problems/binary-tree-maximum-path-sum/binaryTreeMaximumPathSum.js
@@ -1,3 +1,15 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
 var maxPathSum = function(root) {
     let maxPath = Number.MIN_SAFE_INTEGER;
 
