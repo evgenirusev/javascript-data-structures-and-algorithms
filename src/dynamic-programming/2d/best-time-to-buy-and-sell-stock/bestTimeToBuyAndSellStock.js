564f4208b2578d530c4424c0396d5fcb71beddff
evgenirusev <evgeni.n.rusev@gmail.com>
2023-12-29 19:16:20 +0200

    Add 2dp solution with cache

--- src/dynamic-programming/2d/best-time-to-buy-and-sell-stock/bestTimeToBuyAndSellStock.js
+++ src/dynamic-programming/2d/best-time-to-buy-and-sell-stock/bestTimeToBuyAndSellStock.js
@@ -1,4 +1,11 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ * Solution with caching
+ */
 var maxProfit = function(prices) {
+    const cache = {};
+    
     if (prices.length <= 1)
         return 0;
 
@@ -6,26 +13,71 @@ var maxProfit = function(prices) {
         if (i >= prices.length)
             return 0;
 
+        const key = `${i}${cd}${buyIndex}`;
+        if (cache[key] != null) {
+            return cache[key];
+        }
+
+        let result = 0;
         if (cd) {
-            return dfs(i + 1, false, buyIndex);
+            result = dfs(i + 1, false, buyIndex);
         } else if (buyIndex >= 0) {
             let profit = prices[i] - prices[buyIndex];
-            
-            return profit > 0
-                ? profit + dfs(i + 1, true, -1)
-                : dfs(i + 1, true, -1);
+
+            if (profit > 0) {
+                result = Math.max(
+                    profit + dfs(i + 1, true, -1),
+                    dfs(i + 1, false, buyIndex)
+                )
+            } else {
+                result = dfs(i + 1, true, -1);
+            }
         } else {
-            return Math.max(
+            result = Math.max(
                 dfs(i + 1, false, -1),
                 dfs(i + 1, false, i)
             );
         }
+
+        cache[key] = result;
+        return cache[key];
     }
 
     return Math.max(
         dfs(1, false, 0),
-        dfs(1, false, false)
+        dfs(1, false, -1)
     );
 };
+
+// Brute force
+// var maxProfit = function(prices) {
+//     if (prices.length <= 1)
+//         return 0;
+
+//     function dfs(i, cd, buyIndex) {
+//         if (i >= prices.length)
+//             return 0;
+
+//         if (cd) {
+//             return dfs(i + 1, false, buyIndex);
+//         } else if (buyIndex >= 0) {
+//             let profit = prices[i] - prices[buyIndex];
+            
+//             return profit > 0
+//                 ? profit + dfs(i + 1, true, -1)
+//                 : dfs(i + 1, true, -1);
+//         } else {
+//             return Math.max(
+//                 dfs(i + 1, false, -1),
+//                 dfs(i + 1, false, i)
+//             );
+//         }
+//     }
+
+//     return Math.max(
+//         dfs(1, false, 0),
+//         dfs(1, false, false)
+//     );
+// };
 
 module.exports = maxProfit;
